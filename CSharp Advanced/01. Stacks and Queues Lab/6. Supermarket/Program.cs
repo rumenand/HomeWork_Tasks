add7969da881e9169612557b1eb044ab7b7c53ca
rumenand <rumen80@abv.bg>
2019-08-23 23:13:31 +0300

    Task 6 finished

--- CSharp Advanced/01. Stacks and Queues Lab/6. Supermarket/Program.cs
+++ CSharp Advanced/01. Stacks and Queues Lab/6. Supermarket/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _6._Supermarket
 {
@@ -6,7 +7,20 @@ namespace _6._Supermarket
     {
         static void Main()
         {
-           
+            Queue<string> customers = new Queue<string>();
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (input == "Paid")
+                {
+                    while (customers.Count != 0)
+                    {
+                        Console.WriteLine(customers.Dequeue());
+                    }
+                }
+                else customers.Enqueue(input);
+            }
+            Console.WriteLine($"{customers.Count} people remaining.");
         }
     }
 }
