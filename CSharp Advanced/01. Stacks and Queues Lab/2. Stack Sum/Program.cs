3af323a62fba02feb7162921d26d024b42e2e648
rumenand <rumen80@abv.bg>
2019-08-23 10:12:35 +0300

    added task 02. Stack Sum

---
+++ CSharp Advanced/01. Stacks and Queues Lab/2. Stack Sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Stack_Sum
+{
+    class Program
+    {
+        static void Main()
+        {
+            var values = Console.ReadLine().Split().Select(int.Parse);
+            
+            var stack = new Stack<int>(values);
+
+            string input;
+            while ((input = Console.ReadLine().ToLower()) != "end")
+            {
+                string [] commands = input.Split();
+                if (commands[0] == "add")
+                {
+                    stack.Push(int.Parse(commands[1]));
+                    stack.Push(int.Parse(commands[2]));
+                }
+                if (commands[0] == "remove")
+                {
+                    int count = int.Parse(commands[1]);
+                    if (count <= stack.Count)
+                    {
+                        for (int i = 0; i < int.Parse(commands[1]); i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"Sum: {stack.Sum()}");
+        }
+    }
+}
