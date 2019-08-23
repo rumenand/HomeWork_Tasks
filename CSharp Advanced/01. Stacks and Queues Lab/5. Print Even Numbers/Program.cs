8e72a6d5bfae3319d242eff53330cff9ab995993
rumenand <rumen80@abv.bg>
2019-08-23 22:58:17 +0300

    taks 5 finished, but not in judge :(

--- CSharp Advanced/01. Stacks and Queues Lab/5. Print Even Numbers/Program.cs
+++ CSharp Advanced/01. Stacks and Queues Lab/5. Print Even Numbers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _5._Print_Even_Numbers
 {
@@ -6,7 +8,15 @@ namespace _5._Print_Even_Numbers
     {
         static void Main()
         {
-            
+            var numbers = Console.ReadLine().Split().Select(int.Parse);            
+            Queue<int> queue = new Queue<int>(numbers);
+            Queue<int> evens = new Queue<int>();
+            while (queue.Count != 0)
+            {                
+                int current = queue.Dequeue();
+                if (current %2 ==0) evens.Enqueue(current);
+            }
+            Console.WriteLine(string.Join(", ", evens));
         }
     }
 }
