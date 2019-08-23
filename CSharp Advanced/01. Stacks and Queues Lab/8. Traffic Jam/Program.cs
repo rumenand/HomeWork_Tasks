c9c401c0c2c0e7547874cc5fe908e36bdf577a1e
rumenand <rumen80@abv.bg>
2019-08-23 23:31:58 +0300

    task 8 finished

--- CSharp Advanced/01. Stacks and Queues Lab/8. Traffic Jam/Program.cs
+++ CSharp Advanced/01. Stacks and Queues Lab/8. Traffic Jam/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _8._Traffic_Jam
 {
@@ -6,7 +7,25 @@ namespace _8._Traffic_Jam
     {
         static void Main()
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            var queue = new Queue<string>();
+            int count = 0;           
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                if (command == "green")
+                {
+                    int upperLim = Math.Min(n, queue.Count);
+                    for (int i = 0; i< upperLim; i++)
+                    {
+                        Console.WriteLine($"{queue.Dequeue()} passed!");
+                        count++;
+                    }
+                }
+
+              else  queue.Enqueue(command);
+            }
+            Console.WriteLine($"{count} cars passed the crossroads.");
         }
     }
 }
