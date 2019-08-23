379998f698f06d43b158ed25d2dc35cf787f6be3
rumenand <rumen80@abv.bg>
2019-08-23 10:29:00 +0300

    task 4 finished

--- CSharp Advanced/01. Stacks and Queues Lab/4. Matching Brackets/Program.cs
+++ CSharp Advanced/01. Stacks and Queues Lab/4. Matching Brackets/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _4._Matching_Brackets
 {
@@ -6,6 +7,21 @@ namespace _4._Matching_Brackets
     {
         static void Main()
         {
+            var input = Console.ReadLine();
+            var stack = new Stack<int>();
+            for (int i=0;i<input.Length;i++)
+            {
+                if (input[i] == '(') stack.Push(i);
+                if (input[i] == ')')
+                {
+                    for (int j = stack.Pop(); j<=i; j++)
+                    {
+                        Console.Write(input[j]);                        
+                    }
+                    Console.WriteLine();
+                }
+
+            }
             
         }
     }
