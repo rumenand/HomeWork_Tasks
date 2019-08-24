e169eb71536690efc44a0ba4bb97b75018e35126
rumenand <rumen80@abv.bg>
2019-08-24 18:56:11 +0300

    Task 3 finished

--- CSharp Advanced/01.Exercises Stacks and Queues/Problem 3. Maximum and Minimum Element/Program.cs
+++ CSharp Advanced/01.Exercises Stacks and Queues/Problem 3. Maximum and Minimum Element/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Problem_3._Maximum_and_Minimum_Element
 {
@@ -6,7 +8,28 @@ namespace Problem_3._Maximum_and_Minimum_Element
     {
         static void Main()
         {
-           
+            Stack<int> numbers = new Stack<int>();
+            int N = int.Parse(Console.ReadLine());
+            for (int i=0; i<N;i++)
+            {
+                string [] commands = Console.ReadLine().Split();
+                switch (commands[0])
+                {
+                    case "1":
+                        numbers.Push(int.Parse(commands[1]));
+                        break;
+                    case "2":
+                        if (numbers.Count >0) numbers.Pop();
+                        break;
+                    case "3":
+                        if (numbers.Count > 0)  Console.WriteLine(numbers.Max());
+                        break;
+                    case "4":
+                        if (numbers.Count > 0) Console.WriteLine(numbers.Min());
+                        break;
+                }
+            }
+            Console.WriteLine(string.Join(", ",numbers));
         }
     }
 }
