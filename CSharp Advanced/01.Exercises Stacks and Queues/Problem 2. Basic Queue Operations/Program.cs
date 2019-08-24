a90400c37fe98bf21b4c91d174e144088e6f4c2e
rumenand <rumen80@abv.bg>
2019-08-24 18:38:39 +0300

    Task 2 completed!

--- CSharp Advanced/01.Exercises Stacks and Queues/Problem 2. Basic Queue Operations/Program.cs
+++ CSharp Advanced/01.Exercises Stacks and Queues/Problem 2. Basic Queue Operations/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Problem_2._Basic_Queue_Operations
 {
@@ -6,7 +8,18 @@ namespace Problem_2._Basic_Queue_Operations
     {
         static void Main()
         {
-            
+            string[] commands = Console.ReadLine().Split();
+            int N = int.Parse(commands[0]);
+            int S = int.Parse(commands[1]);
+            int X = int.Parse(commands[2]);
+            Queue<int> numbers = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            for (int i = 0; i < S; i++)
+            {
+                numbers.Dequeue();
+            }
+            if (numbers.Count == 0) Console.WriteLine("0");
+            else if (numbers.Contains(X)) Console.WriteLine("true");
+            else Console.WriteLine(numbers.Min());
         }
     }
 }
