4399eaf4aceb790653abd72140e608f216bf89df
rumenand <rumen80@abv.bg>
2019-08-24 14:26:36 +0300

    added new project

---
+++ CSharp Advanced/01.Exercises Stacks and Queues/01.Exercises Stacks and Queues/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.Exercises_Stacks_and_Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
