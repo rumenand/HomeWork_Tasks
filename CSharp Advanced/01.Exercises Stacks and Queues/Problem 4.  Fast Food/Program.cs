e77058bd897292d8bb1d212c12187eed0d548627
rumenand <rumen80@abv.bg>
2019-08-24 19:15:23 +0300

    Task 4 completed

--- CSharp Advanced/01.Exercises Stacks and Queues/Problem 4.  Fast Food/Program.cs
+++ CSharp Advanced/01.Exercises Stacks and Queues/Problem 4.  Fast Food/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Problem_4.__Fast_Food
 {
@@ -6,6 +8,21 @@ namespace Problem_4.__Fast_Food
     {
         static void Main()
         {
+            int foodQuantity = int.Parse(Console.ReadLine());            
+            Queue<int> orders = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            Console.WriteLine(orders.Max());
+            while(orders.Count>0)
+            {
+                int newOrder = orders.Peek();
+                if ((foodQuantity - newOrder) > -1)
+                {
+                    foodQuantity -= newOrder;
+                    orders.Dequeue();
+                }
+                else break;
+            }
+            if (orders.Count > 0) Console.WriteLine($"Orders left: {string.Join(" ",orders)}");
+            else Console.WriteLine("Orders complete");
             
         }
     }
