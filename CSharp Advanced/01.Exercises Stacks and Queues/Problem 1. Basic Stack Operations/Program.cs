c03f8acb59d48e33618fcef80a713f7e799d4254
rumenand <rumen80@abv.bg>
2019-08-24 18:30:43 +0300

    task 1 completed

--- CSharp Advanced/01.Exercises Stacks and Queues/Problem 1. Basic Stack Operations/Program.cs
+++ CSharp Advanced/01.Exercises Stacks and Queues/Problem 1. Basic Stack Operations/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Problem_1._Basic_Stack_Operations
 {
@@ -6,7 +8,18 @@ namespace Problem_1._Basic_Stack_Operations
     {
         static void Main()
         {
-           
+            string[] commands = Console.ReadLine().Split();
+            int N = int.Parse(commands[0]);
+            int S = int.Parse(commands[1]);
+            int X = int.Parse(commands[2]);
+            Stack<int> numbers = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            for (int i=0; i<S;i++)
+            {
+                numbers.Pop();
+            }
+            if (numbers.Count == 0) Console.WriteLine("0");
+            else if (numbers.Contains(X)) Console.WriteLine("true");
+            else Console.WriteLine(numbers.Min());
         }
     }
 }
