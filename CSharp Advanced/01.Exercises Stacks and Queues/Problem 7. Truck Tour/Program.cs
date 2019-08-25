87f99f2e028180f0b866e963f558459e9817b011
rumenand <rumen80@abv.bg>
2019-08-25 10:02:00 +0300

    Added task 7

---
+++ CSharp Advanced/01.Exercises Stacks and Queues/Problem 7. Truck Tour/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_7._Truck_Tour
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
