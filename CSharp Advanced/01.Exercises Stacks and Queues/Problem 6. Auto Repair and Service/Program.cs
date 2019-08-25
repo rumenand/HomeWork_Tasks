e836ca2299761414737747bcc69b9a78ad30f3e1
rumenand <rumen80@abv.bg>
2019-08-25 10:00:52 +0300

    task 6 completed

--- CSharp Advanced/01.Exercises Stacks and Queues/Problem 6. Auto Repair and Service/Program.cs
+++ CSharp Advanced/01.Exercises Stacks and Queues/Problem 6. Auto Repair and Service/Program.cs
@@ -15,18 +15,25 @@ namespace Problem_6._Auto_Repair_and_Service
                 string[] commands = input.Split("-");
                 if (commands[0] == "Service")
                 {
-
+                    if (vehiclesForService.Count > 0)
+                    {
+                        string newServicedVehicle = vehiclesForService.Dequeue();
+                        Console.WriteLine($"Vehicle {newServicedVehicle} got served.");
+                        servicedVehicles.Push(newServicedVehicle);
+                    }
                 }
                 else if (commands[0] == "CarInfo")
                 {
-
+                    if (vehiclesForService.Contains(commands[1])) Console.WriteLine("Still waiting for service.");
+                    else if (servicedVehicles.Contains(commands[1])) Console.WriteLine("Served.");
                 }
                 else if (commands[0] == "History")
                 {
-
+                    Console.WriteLine(string.Join(", ",servicedVehicles));
                 }
             }
-            
+            if (vehiclesForService.Count >0) Console.WriteLine($"Vehicles for service: {string.Join(", ",vehiclesForService)}");
+            Console.WriteLine($"Served vehicles: {string.Join(", ", servicedVehicles)}");
         }
     }
 }
