0384bb9de26188e10fd1d908ffefa9b375fe5556
rumenand <rumen80@abv.bg>
2019-08-25 09:33:37 +0300

    Task 5 finished

--- CSharp Advanced/01.Exercises Stacks and Queues/Problem 5. Fashion Boutique/Program.cs
+++ CSharp Advanced/01.Exercises Stacks and Queues/Problem 5. Fashion Boutique/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Problem_5._Fashion_Boutique
 {
@@ -7,7 +8,21 @@ namespace Problem_5._Fashion_Boutique
     {
         static void Main()
         {
-            
+            Stack<int> clothes = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            int RackCapacity = int.Parse(Console.ReadLine());
+            int rackCount = 1;
+            int currentRackCapacity = 0;
+            while (clothes.Count >0)
+            {
+                int next = clothes.Pop();
+                if ((currentRackCapacity + next) <= RackCapacity) currentRackCapacity += next;
+                else
+                {
+                    rackCount++;
+                    currentRackCapacity = next;
+                }
+            }
+            Console.WriteLine(rackCount);
         }
     }
 }
