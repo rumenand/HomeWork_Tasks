854f2e055ac7e0dede623cc4302cb71cb1c91b94
rumenand <rumen80@abv.bg>
2019-08-25 22:19:05 +0300

    Tas 8 finished

--- CSharp Advanced/01.Exercises Stacks and Queues/Problem 8. Balanced Parentheses/Program.cs
+++ CSharp Advanced/01.Exercises Stacks and Queues/Problem 8. Balanced Parentheses/Program.cs
@@ -7,7 +7,34 @@ namespace Problem_8._Balanced_Parentheses
     {
         static void Main()
         {
-            
+            string input = Console.ReadLine();
+            bool balanced = true;
+            var stack = new Stack<char>();
+            foreach (char ch in input)
+            {
+                switch (ch)
+                {
+                    case '{':
+                    case '(':
+                    case '[':
+                        stack.Push(ch);
+                        break;
+                    case '}':
+                        if (stack.Count != 0 && stack.Pop() == '{') balanced = true;
+                        else balanced = false;
+                        break;
+                    case ')':
+                        if (stack.Count != 0 && stack.Pop() == '(') balanced = true;
+                        else balanced = false;
+                        break;
+                    case ']':
+                        if (stack.Count != 0 && stack.Pop() == '[') balanced = true;
+                        else balanced = false;
+                        break;
+                }
+                if (!balanced) break;
+            }
+            Console.WriteLine(balanced ? "YES" : "NO");
         }
     }
 }
