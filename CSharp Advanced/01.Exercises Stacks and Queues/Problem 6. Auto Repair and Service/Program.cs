a9703a1a3554e64fb85c6a5ae75c31865395db73
rumenand <rumen80@abv.bg>
2019-08-25 09:35:27 +0300

    Task 6 added empty

---
+++ CSharp Advanced/01.Exercises Stacks and Queues/Problem 6. Auto Repair and Service/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_6._Auto_Repair_and_Service
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
