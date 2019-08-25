c686de78ab4a11989b8129b46bb341ae4803368e
rumenand <rumen80@abv.bg>
2019-08-25 20:35:50 +0300

    Task 7 finished but needs improvement on the logic.

--- CSharp Advanced/01.Exercises Stacks and Queues/Problem 7. Truck Tour/Program.cs
+++ CSharp Advanced/01.Exercises Stacks and Queues/Problem 7. Truck Tour/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Problem_7._Truck_Tour
 {
@@ -7,7 +8,41 @@ namespace Problem_7._Truck_Tour
     {
         static void Main()
         {
+            int N = int.Parse(Console.ReadLine());
+            var circle = new Queue<string>();
+            for (int i=0; i<N; i++)
+            {
+                circle.Enqueue(i+" "+Console.ReadLine());               
+            }
             
+            while (circle.Count > 0)
+            {
+                bool found = true;
+                int[] first = circle.Dequeue().Split().Select(int.Parse).ToArray();
+                int fuel = first[1];
+                int distance = first[2];
+                if (fuel > distance)
+                {
+                    foreach (var station in circle)
+                    {
+                        fuel -= distance;
+                        int[] next = station.Split().Select(int.Parse).ToArray();
+                        distance = next[2];
+                        fuel += next[1];
+                        if (fuel < distance)
+                        {
+                            found = false;
+                            break;
+                        }
+                    }
+                    if (found)
+                    {
+                        Console.WriteLine(first[0]);
+                        break;
+                    }
+
+                }
+            }
         }
     }
 }
