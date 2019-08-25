95c93e82135619ea53705d4624033e7d6ec32c38
rumenand <rumen80@abv.bg>
2019-08-25 09:22:09 +0300

    task 5 finished but not in judge. Changed tasks file conditions with updated

--- CSharp Advanced/01.Exercises Stacks and Queues/Problem 5. Calculate Sequence with Queue/Program.cs
+++ CSharp Advanced/01.Exercises Stacks and Queues/Problem 5. Calculate Sequence with Queue/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Problem_5._Calculate_Sequence_with_Queue
 {
@@ -6,7 +7,38 @@ namespace Problem_5._Calculate_Sequence_with_Queue
     {
         static void Main()
         {
-            
+            int N = int.Parse(Console.ReadLine());
+            Queue<int> numbers = new Queue<int>();
+            numbers.Enqueue(N);
+            Console.Write($"{N} ");
+            int count = 1;
+            int oper = 1;
+            while (count<51)
+            {
+                int next = numbers.Peek();
+                int seqMember = 0;
+                switch (oper)
+                {
+                    case 1:
+                        seqMember = next + 1;
+                        break;
+                    case 2:
+                        seqMember = 2*next + 1;
+                        break;
+                    case 3:
+                        seqMember = next + 2;
+                        break;
+                }
+                numbers.Enqueue(seqMember);
+                Console.Write($"{seqMember} ");
+                count++;
+                oper++;
+                if (oper >3)
+                {
+                    oper = 1;
+                    numbers.Dequeue();
+                }
+            }
         }
     }
 }
