c0b5630e1a22018d8cae3793f0e84ffedb2ab63b
rumenand <rumen80@abv.bg>
2019-08-26 17:36:07 +0300

    task 9 added undo stack for case 1

--- CSharp Advanced/01.Exercises Stacks and Queues/Problem 9. Simple Text Editor/Program.cs
+++ CSharp Advanced/01.Exercises Stacks and Queues/Problem 9. Simple Text Editor/Program.cs
@@ -10,6 +10,7 @@ namespace Problem_9._Simple_Text_Editor
         {
             int lines = int.Parse(Console.ReadLine());
             StringBuilder sb = new StringBuilder();
+            var undoStack = new Stack<string>();
             for (int i=0; i<lines;i++)
             {
                 string[] commands = Console.ReadLine().Split();
@@ -17,13 +18,16 @@ namespace Problem_9._Simple_Text_Editor
                 {
                     case "1":
                         sb.Append(commands[1]);
+                        undoStack.Push("2 " + commands[1].Length);
                         break;
-                    case "2":
+                    case "2":                        
+                        //sb.remove
                         break;
                     case "3":
                         Console.WriteLine(sb[int.Parse(commands[1])]);
                         break;
                     case "4":
+                        // undo
                         break;
                 }
             }
