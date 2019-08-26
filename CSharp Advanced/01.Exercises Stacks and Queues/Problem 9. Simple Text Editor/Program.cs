6b8b504b1135208b01b659a7efc5206dfea82cbd
rumenand <rumen80@abv.bg>
2019-08-26 12:18:17 +0300

    task 9 logic to case 1 - add text to the end

--- CSharp Advanced/01.Exercises Stacks and Queues/Problem 9. Simple Text Editor/Program.cs
+++ CSharp Advanced/01.Exercises Stacks and Queues/Problem 9. Simple Text Editor/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Problem_9._Simple_Text_Editor
 {
@@ -8,12 +9,14 @@ namespace Problem_9._Simple_Text_Editor
         static void Main()
         {
             int lines = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
             for (int i=0; i<lines;i++)
             {
                 string[] commands = Console.ReadLine().Split();
                 switch (commands[0])
                 {
                     case "1":
+                        sb.Append(commands[1]);
                         break;
                     case "2":
                         break;
