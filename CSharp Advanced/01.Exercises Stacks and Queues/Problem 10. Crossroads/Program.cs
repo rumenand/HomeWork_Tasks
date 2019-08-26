58cdc8f158a19534bcab879a5d6e53873d9fee85
rumenand <rumen80@abv.bg>
2019-08-26 21:45:22 +0300

    Taks 10 finished

--- CSharp Advanced/01.Exercises Stacks and Queues/Problem 10. Crossroads/Program.cs
+++ CSharp Advanced/01.Exercises Stacks and Queues/Problem 10. Crossroads/Program.cs
@@ -7,14 +7,44 @@ namespace Problem_10._Crossroads
     {
         static void Main()
         {
-            int greenLDuratino = int.Parse(Console.ReadLine());
+            int greenLDuration = int.Parse(Console.ReadLine());
             int freeWindow = int.Parse(Console.ReadLine());
+            var carsTail = new Queue<string>();
             string input;
+            int totalPassed = 0;
+            bool crashHappened = false;
             while((input = Console.ReadLine()) != "END")
             {
-
+                if (input == "green")
+                {
+                    int greenLight = greenLDuration;
+                    string nextCar = "";
+                    while (greenLight>0 && carsTail.Count > 0)
+                    {
+                        nextCar = carsTail.Dequeue();
+                        totalPassed++;
+                        greenLight -= nextCar.Length;
+                    }
+                    if (greenLight < 0)
+                    {
+                        greenLight += nextCar.Length;
+                        int crashIndex = nextCar.Length - (greenLight + freeWindow);
+                        if (crashIndex > 0)
+                        {
+                            Console.WriteLine("A crash happened!");
+                            Console.WriteLine($"{nextCar} was hit at { nextCar[greenLight + freeWindow]}.");
+                            crashHappened = true;
+                            break;
+                        }                        
+                    }
+                }
+                else carsTail.Enqueue(input);
             }
-
+            if (!crashHappened)
+            {
+                Console.WriteLine("Everyone is safe.");
+                Console.WriteLine($"{totalPassed} total cars passed the crossroads.");
+            }
         }
     }
 }
