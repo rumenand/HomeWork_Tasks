e526d802506f1bcc25e29534ae7720b81c9f7b36
rumenand <rumen80@abv.bg>
2019-08-26 12:14:27 +0300

    Task 9 added functionality

--- CSharp Advanced/01.Exercises Stacks and Queues/Problem 9. Simple Text Editor/Program.cs
+++ CSharp Advanced/01.Exercises Stacks and Queues/Problem 9. Simple Text Editor/Program.cs
@@ -7,7 +7,22 @@ namespace Problem_9._Simple_Text_Editor
     {
         static void Main()
         {
-            
+            int lines = int.Parse(Console.ReadLine());
+            for (int i=0; i<lines;i++)
+            {
+                string[] commands = Console.ReadLine().Split();
+                switch (commands[0])
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                }
+            }
         }
     }
 }
