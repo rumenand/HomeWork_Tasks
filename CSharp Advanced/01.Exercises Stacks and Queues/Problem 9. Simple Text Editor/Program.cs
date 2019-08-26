c11d55813cd77000d1c5ad610ad9985f9c9ee553
rumenand <rumen80@abv.bg>
2019-08-26 12:10:31 +0300

    Task 9 added empty

---
+++ CSharp Advanced/01.Exercises Stacks and Queues/Problem 9. Simple Text Editor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_9._Simple_Text_Editor
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
