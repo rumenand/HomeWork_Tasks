e02e7aa34b2f80208ebc95e341f8c133098950c0
rumenand <rumen80@abv.bg>
2019-08-27 07:56:56 +0300

    Task 11 added functionality

--- CSharp Advanced/01.Exercises Stacks and Queues/Problem 11. Key Revolver/Program.cs
+++ CSharp Advanced/01.Exercises Stacks and Queues/Problem 11. Key Revolver/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Problem_11._Key_Revolver
 {
@@ -7,7 +8,15 @@ namespace Problem_11._Key_Revolver
     {
         static void Main()
         {
-            
+            int bulletPrice = int.Parse(Console.ReadLine());
+            int barrelSize = int.Parse(Console.ReadLine());
+            var bullets = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            var locks = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            int intelligence = int.Parse(Console.ReadLine());
+            while (bullets.Count>0 && locks.Count >0)
+            {
+
+            }
         }
     }
 }
