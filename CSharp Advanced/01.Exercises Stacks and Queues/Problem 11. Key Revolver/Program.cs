e2a8e49f4586a841c9e2fbbca411ef791dfdf7f7
rumenand <rumen80@abv.bg>
2019-08-27 08:14:34 +0300

    Task 11 done on 80%. Need to fix a Reloading! issue

--- CSharp Advanced/01.Exercises Stacks and Queues/Problem 11. Key Revolver/Program.cs
+++ CSharp Advanced/01.Exercises Stacks and Queues/Problem 11. Key Revolver/Program.cs
@@ -14,13 +14,26 @@ namespace Problem_11._Key_Revolver
             var locks = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
             int initialBullets = bullets.Count;
             int intelligence = int.Parse(Console.ReadLine());
+            int shot = 0;
             while (bullets.Count>0 && locks.Count >0)
             {
-                int p= bullets.Pop();
-                locks.Dequeue();
+                int currentBullet= bullets.Pop();                
+                int currentLock = locks.Peek();
+                if (currentBullet<=currentLock)
+                {
+                    locks.Dequeue();
+                    Console.WriteLine("Bang!");
+                }
+                else Console.WriteLine("Ping!");
+                shot++;
+                if (shot==barrelSize)
+                {
+                    Console.WriteLine("Reloading!");
+                    shot = 0;
+                }
             }        
             int bulletsPrice = (initialBullets - bullets.Count) * bulletPrice;
-            if (locks.Count > 0) Console.WriteLine($"{bullets.Count} bullets left. Earned ${intelligence-bulletsPrice}");
+            if (locks.Count == 0) Console.WriteLine($"{bullets.Count} bullets left. Earned ${intelligence-bulletsPrice}");
             else Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
         }
     }
