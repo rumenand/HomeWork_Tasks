aabf42779030b896a577b9d41e080e7af691e407
rumenand <rumen80@abv.bg>
2019-08-28 21:48:26 +0300

    Task 12 finished but works wrong.

--- CSharp Advanced/01.Exercises Stacks and Queues/Problem 12. Cups and Bottles/Program.cs
+++ CSharp Advanced/01.Exercises Stacks and Queues/Problem 12. Cups and Bottles/Program.cs
@@ -11,10 +11,30 @@ namespace Problem_12._Cups_and_Bottles
             var cups = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
             var bottles = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
             int wastedWater = 0;
-            while (bottles.Count > 0)
+            while (bottles.Count > 0 && cups.Count>0)
             {
-                int currentBottle = bottles.Dequeue();
-
+                int currentCup = cups.Pop();
+                bool filled = false;
+                while(!filled && bottles.Count >0)
+                {
+                    int currentBottle = bottles.Dequeue();
+                    currentCup -= currentBottle;
+                    if (currentCup<=0)
+                    {
+                        filled = true;
+                        wastedWater += currentCup * -1;
+                    }
+                }
+            }
+            if (bottles.Count > 0)
+            {
+                Console.WriteLine($"Bottles: {string.Join(" ",bottles)}");
+                Console.WriteLine($"Wasted litters of water: {wastedWater}.");
+            }
+            else
+            {
+                Console.WriteLine($"Cups: {string.Join(" ", cups)}");
+                Console.WriteLine($"Wasted litters of water: {wastedWater}.");
             }
         }
     }
