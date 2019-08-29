158159447db0b44a9254133ec1220c5f8b8ff34f
rumenand <rumen80@abv.bg>
2019-08-29 12:54:10 +0300

    Lab Multidimension added

---
+++ CSharp Advanced/02. Lab Multidimensional Arrays/02. Lab Multidimensional Arrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02._Lab_Multidimensional_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
