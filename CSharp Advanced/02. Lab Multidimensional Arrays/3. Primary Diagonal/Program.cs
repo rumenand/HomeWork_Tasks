68b583efa1581bfaf250b53f54fc75fb98bcdef5
rumenand <rumen80@abv.bg>
2019-08-31 23:42:00 +0300

    Task 3 done on test examples

--- CSharp Advanced/02. Lab Multidimensional Arrays/3. Primary Diagonal/Program.cs
+++ CSharp Advanced/02. Lab Multidimensional Arrays/3. Primary Diagonal/Program.cs
@@ -6,7 +6,22 @@ namespace _3._Primary_Diagonal
     {
         static void Main()
         {
-            
+            int matrixSize = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[matrixSize, matrixSize];
+            for (int i=0; i<matrixSize;i++)
+            {
+                string[] row = Console.ReadLine().Split();
+                for (int j=0;j<matrixSize;j++)
+                {
+                    matrix[i, j] = int.Parse(row[j]);
+                }
+            }
+            int diagonalSum = 0;
+            for (int i=0;i<matrixSize;i++)
+            {
+                diagonalSum += matrix[i, i];
+            }
+            Console.WriteLine(diagonalSum);
         }
     }
 }
