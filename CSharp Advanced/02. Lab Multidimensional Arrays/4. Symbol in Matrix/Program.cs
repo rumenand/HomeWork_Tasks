119efc032c0d7d9f636938db6973e7c9e32e65db
rumenand <rumen80@abv.bg>
2019-09-01 20:05:45 +0300

    Taks 4 added input part

--- CSharp Advanced/02. Lab Multidimensional Arrays/4. Symbol in Matrix/Program.cs
+++ CSharp Advanced/02. Lab Multidimensional Arrays/4. Symbol in Matrix/Program.cs
@@ -6,7 +6,16 @@ namespace _4._Symbol_in_Matrix
     {
         static void Main()
         {
-          
+            int matrixSize = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[matrixSize, matrixSize];
+            for (int i = 0; i < matrixSize; i++)
+            {
+                string row = Console.ReadLine();
+                for (int j = 0; j < matrixSize; j++)
+                {
+                    matrix[i, j] = row[j];
+                }
+            }
         }
     }
 }
