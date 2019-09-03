8a8c305d652407f8b1a3ece6da115d55feceb394
rumenand <rumen80@abv.bg>
2019-09-03 23:39:51 +0300

    Task 5 finished on 0 tests

--- CSharp Advanced/02. Lab Multidimensional Arrays/5. Square with Maximum Sum/Program.cs
+++ CSharp Advanced/02. Lab Multidimensional Arrays/5. Square with Maximum Sum/Program.cs
@@ -18,6 +18,24 @@ namespace _5._Square_with_Maximum_Sum
                     matrix[i, j] = int.Parse(row[j]);
                 }
             }
+            int maxSum2x2Mat = 0;
+            int[] index = new int[2];
+            for (int i = 0; i < matrixRows - 1; i++)
+            {
+                for (int j = 0; j < matrixColumns - 1; j++)
+                {
+                    int currentSum = matrix[i, j] + matrix[i, j + 1] + matrix[i + 1, j] + matrix[i + 1, j + 1];
+                    if (currentSum > maxSum2x2Mat)
+                    {
+                        maxSum2x2Mat = currentSum;
+                        index[0] = i;
+                        index[1] = j;
+                    }
+                }
+            }
+            Console.WriteLine($"{matrix[index[0],index[1]]} {matrix[index[0], index[1]+1]}");
+            Console.WriteLine($"{matrix[index[0]+1, index[1]]} {matrix[index[0]+1, index[1] + 1]}");
+            Console.WriteLine(maxSum2x2Mat);
         }
     }
 }
