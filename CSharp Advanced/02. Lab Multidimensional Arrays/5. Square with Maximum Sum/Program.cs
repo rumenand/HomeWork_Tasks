164909f9c902da74dd5fbc06f300de17c472cba8
rumenand <rumen80@abv.bg>
2019-09-03 23:29:22 +0300

    Task 5 added input

--- CSharp Advanced/02. Lab Multidimensional Arrays/5. Square with Maximum Sum/Program.cs
+++ CSharp Advanced/02. Lab Multidimensional Arrays/5. Square with Maximum Sum/Program.cs
@@ -6,7 +6,18 @@ namespace _5._Square_with_Maximum_Sum
     {
         static void Main()
         {
-            
+            string [] input = Console.ReadLine().Split(",",StringSplitOptions.RemoveEmptyEntries);
+            int matrixRows = int.Parse(input[0]);
+            int matrixColumns = int.Parse(input[1]);
+            int[,] matrix = new int[matrixRows, matrixColumns];
+            for (int i=0;i<matrixRows;i++)
+            {
+                string[] row = Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries);
+                for (int j=0;j<matrixColumns;j++)
+                {
+                    matrix[i, j] = int.Parse(row[j]);
+                }
+            }
         }
     }
 }
