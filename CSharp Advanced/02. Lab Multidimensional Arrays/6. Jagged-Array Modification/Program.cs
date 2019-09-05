626c75eff0db3b448f9b21ac08019f74291620bb
rumenand <rumen80@abv.bg>
2019-09-05 23:11:06 +0300

    Task 6 done on 0 tests

--- CSharp Advanced/02. Lab Multidimensional Arrays/6. Jagged-Array Modification/Program.cs
+++ CSharp Advanced/02. Lab Multidimensional Arrays/6. Jagged-Array Modification/Program.cs
@@ -22,13 +22,24 @@ namespace _6._Jagged_Array_Modification
                 int row = int.Parse(tokens[1]);
                 int col = int.Parse(tokens[2]);
                 int value = int.Parse(tokens[3]);
-                if (row < 0 || row >= matrix.Length) Console.WriteLine("Invalid coordinates");
+                if (row < 0 || row >= matrix.Length || col <0 || col>=matrix[row].Length) Console.WriteLine("Invalid coordinates");
                 else
                 {
-                    //TODO: Execute the command 
+                    switch (command)
+                    {
+                        case "Add":
+                            matrix[row][col] += value;
+                            break;
+                        case "Subtract":
+                            matrix[row][col] -= value;
+                            break;
+                    }
                 }
             }
-                //TODO: Print the matrix
+             for (int i=0;i<rows;i++)
+            {
+                Console.WriteLine(string.Join(" ",matrix[i]));
+            }
                 
         }
     }
