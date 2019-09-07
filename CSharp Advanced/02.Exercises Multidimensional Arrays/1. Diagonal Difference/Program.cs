d0198a44a20d9f06fb88a252be6630597e978360
rumenand <rumen80@abv.bg>
2019-09-07 21:56:41 +0300

    Added exercises Multidimensional arrays.

---
+++ CSharp Advanced/02.Exercises Multidimensional Arrays/1. Diagonal Difference/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Diagonal_Difference
+{
+    class Program
+    {
+        static void Main()
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+            int [][] matrix = new int [matrixSize][];
+            for (int i=0;i<matrixSize;i++)
+            {
+                var input = Console.ReadLine().Split().Select(int.Parse);
+                matrix[i] = input.ToArray();
+            }
+        }
+    }
+}
