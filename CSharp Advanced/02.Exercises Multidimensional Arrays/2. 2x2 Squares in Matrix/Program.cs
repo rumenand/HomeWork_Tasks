4cf16186d9e4466015b90ac52fe9d6a10ead98c5
rumenand <rumen80@abv.bg>
2019-09-08 19:44:08 +0300

    Task 2 done on 0 tests.

--- CSharp Advanced/02.Exercises Multidimensional Arrays/2. 2x2 Squares in Matrix/Program.cs
+++ CSharp Advanced/02.Exercises Multidimensional Arrays/2. 2x2 Squares in Matrix/Program.cs
@@ -17,6 +17,18 @@ namespace _2._2x2_Squares_in_Matrix
                     matrix[i, j] = input[j];
                 }
             }
+            int equalSquare = 0;
+            for (int i=0;i<matrixSize[0]-1;i++)
+            {
+                for (int j=0;j<matrixSize[1]-1;j++)
+                {
+                    if (matrix[i, j] == matrix[i + 1, j]
+                        && matrix[i, j] == matrix[i, j + 1]
+                        && matrix[i, j] == matrix[i + 1, j + 1])
+                        equalSquare++;
+                }
+            }
+            Console.WriteLine(equalSquare);
         }
     }
 }
