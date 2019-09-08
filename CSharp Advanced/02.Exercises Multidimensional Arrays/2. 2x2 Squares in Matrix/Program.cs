b8882b9bc88b71e41cb39b0860d5b346aa90c896
rumenand <rumen80@abv.bg>
2019-09-08 20:01:56 +0300

    Task 2 passed 100% but with strings, not chars.

--- CSharp Advanced/02.Exercises Multidimensional Arrays/2. 2x2 Squares in Matrix/Program.cs
+++ CSharp Advanced/02.Exercises Multidimensional Arrays/2. 2x2 Squares in Matrix/Program.cs
@@ -8,13 +8,13 @@ namespace _2._2x2_Squares_in_Matrix
         static void Main()
         {
             int[] matrixSize = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            char[,] matrix = new char [matrixSize[0], matrixSize[1]];
+            string[,] matrix = new string [matrixSize[0], matrixSize[1]];
             for (int i=0; i<matrixSize[0];i++)
             {
-                char [] input = Console.ReadLine().Split().Select(char.Parse).ToArray();
+              string [] input = Console.ReadLine().Split();
                 for (int j=0;j<matrixSize[1];j++)
                 {
-                    matrix[i, j] = input[j];
+                   matrix[i, j] = input[j];
                 }
             }
             int equalSquare = 0;
