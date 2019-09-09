31fa0354c3543baf81f176acf4e1f99a631fb193
rumenand <rumen80@abv.bg>
2019-09-09 23:21:11 +0300

    Task 4 added input.

--- CSharp Advanced/02.Exercises Multidimensional Arrays/4. Matrix shuffling/Program.cs
+++ CSharp Advanced/02.Exercises Multidimensional Arrays/4. Matrix shuffling/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _4._Matrix_shuffling
 {
@@ -6,7 +7,28 @@ namespace _4._Matrix_shuffling
     {
         static void Main()
         {
-            
-        }
+            int[] matrixSize = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[,] matrix = new int[matrixSize[0], matrixSize[1]];
+            bool converted = true;
+            for (int i = 0; i < matrixSize[0]; i++)
+            {
+                var input = Console.ReadLine().Split().ToArray();
+                for (int j = 0; j < matrixSize[1]; j++)
+                {
+                    int number;
+                    bool success = int.TryParse(input[j], out number);
+                    if (success) matrix[i, j] = number;
+                    else
+                    {
+                        converted = false;
+                        break;
+                    }
+                }
+            }
+            if (converted)
+            {
+
+            }
+            }
     }
 }
