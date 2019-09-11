87379bbd077b2a938d5607c0821bdc5cc1c9ed45
rumenand <rumen80@abv.bg>
2019-09-11 19:31:25 +0300

    Task 6 added bomb and print functions.

--- CSharp Advanced/02.Exercises Multidimensional Arrays/6. Bomb the Basement/Program.cs
+++ CSharp Advanced/02.Exercises Multidimensional Arrays/6. Bomb the Basement/Program.cs
@@ -10,6 +10,33 @@ namespace _6._Bomb_the_Basement
             int[] matrixSize = Console.ReadLine().Split().Select(int.Parse).ToArray();
             int[,] matrix = new int[matrixSize[0], matrixSize[1]];
             int[] bombParam = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            BoomAction(matrix, bombParam);
+           // GravityFall(matrix);
+            PrintMatrix(matrix);
+        }
+
+        static void BoomAction(int[,] matrix, int [] bomb)
+        {
+            for (int i=0; i<matrix.GetLength(0); i++)
+            {
+                for (int j=0; j<matrix.GetLength(1); j++)
+                {
+                    double distamceToBomb = Math.Sqrt(Math.Pow((bomb[0] - i), 2) + Math.Pow((bomb[1] - j), 2));
+                    if (distamceToBomb <= bomb[2]) matrix[i, j] = 1;
+                }
+            }
+        }
+
+        static void PrintMatrix(int [,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
