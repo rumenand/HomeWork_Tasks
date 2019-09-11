7e868fb162c9dcc0c6ae2d269ce4fd0c3378edeb
rumenand <rumen80@abv.bg>
2019-09-11 09:36:22 +0300

    Task 6 added input.

--- CSharp Advanced/02.Exercises Multidimensional Arrays/6. Bomb the Basement/Program.cs
+++ CSharp Advanced/02.Exercises Multidimensional Arrays/6. Bomb the Basement/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _6._Bomb_the_Basement
 {
@@ -6,7 +7,9 @@ namespace _6._Bomb_the_Basement
     {
         static void Main()
         {
-            
+            int[] matrixSize = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[,] matrix = new int[matrixSize[0], matrixSize[1]];
+            int[] bombParam = Console.ReadLine().Split().Select(int.Parse).ToArray();
         }
     }
 }
