d77a5be5a15109286014cc44186b4215d93f8b54
rumenand <rumen80@abv.bg>
2019-09-12 19:16:01 +0300

    Task 7 added input.

--- CSharp Advanced/02.Exercises Multidimensional Arrays/7.  Knight Game/Program.cs
+++ CSharp Advanced/02.Exercises Multidimensional Arrays/7.  Knight Game/Program.cs
@@ -6,7 +6,16 @@ namespace _7.__Knight_Game
     {
         static void Main()
         {
-            
+            int size = int.Parse(Console.ReadLine());
+            char[,] board = new char[size, size];
+            for (int i=0; i<size; i++)
+            {
+                string row = Console.ReadLine();
+                for (int j=0; j<size; j++)
+                {
+                    board[i, j] = row[j];
+                }
+            }
         }
     }
 }
