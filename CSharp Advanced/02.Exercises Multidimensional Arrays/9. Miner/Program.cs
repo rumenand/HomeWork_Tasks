696bbe2f81786ecce0ca1786e1d7e48f849a76ca
rumenand <rumen80@abv.bg>
2019-09-13 08:20:07 +0300

    Task 9 added input.

--- CSharp Advanced/02.Exercises Multidimensional Arrays/9. Miner/Program.cs
+++ CSharp Advanced/02.Exercises Multidimensional Arrays/9. Miner/Program.cs
@@ -6,7 +6,17 @@ namespace _9._Miner
     {
         static void Main()
         {
-            
+            int size = int.Parse(Console.ReadLine());
+            char[,] field = new char[size, size];
+            string[] commands = Console.ReadLine().Split();
+            for (int i = 0; i < size; i++)
+            {
+                string [] row = Console.ReadLine().Split();
+                for (int j = 0; j < size; j++)
+                {
+                    field[i, j] = char.Parse(row[j]);
+                }
+            }
         }
     }
 }
