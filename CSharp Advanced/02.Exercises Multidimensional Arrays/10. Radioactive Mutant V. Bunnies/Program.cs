cd07f8fdfa1a5d24afaeac84db555bd42df88ea7
rumenand <rumen80@abv.bg>
2019-09-13 12:28:51 +0300

    Task 10 added input.

--- CSharp Advanced/02.Exercises Multidimensional Arrays/10. Radioactive Mutant V. Bunnies/Program.cs
+++ CSharp Advanced/02.Exercises Multidimensional Arrays/10. Radioactive Mutant V. Bunnies/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _10._Radioactive_Mutant_V._Bunnies
 {
@@ -6,7 +7,17 @@ namespace _10._Radioactive_Mutant_V._Bunnies
     {
         static void Main()
         {
-            
+            int[] matrixSize = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            char[,] matrix = new char[matrixSize[0], matrixSize[1]];
+            for (int i = 0; i < matrixSize[0]; i++)
+            {
+                string row = Console.ReadLine();
+                for (int j = 0; j < matrixSize[1]; j++)
+                {
+                    matrix[i, j] = row[j];
+                }
+            }
+            string command = Console.ReadLine();
         }
     }
 }
