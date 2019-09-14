1ceb7aa8b4849e14b77061cf30205ae36e5370c0
rumenand <rumen80@abv.bg>
2019-09-14 15:31:47 +0300

    Task 6 done on 100%.

--- CSharp Advanced/03.Lab Sets and Dictionaries Advanced/6. Parking Lot/Program.cs
+++ CSharp Advanced/03.Lab Sets and Dictionaries Advanced/6. Parking Lot/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _6._Parking_Lot
 {
     class Program
     {
         static void Main()
-        {
-            
+        {            
+            var parking = new HashSet<string>();
+            string input;
+            while ((input = Console.ReadLine()) !="END")
+            {
+                string[] commands = input.Split(", ");
+                if (commands[0] == "IN") parking.Add(commands[1]);
+                else parking.Remove(commands[1]);
+            }
+            if (parking.Count>0)
+            {
+                foreach (var car in parking)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+            else Console.WriteLine("Parking Lot is Empty");
         }
     }
 }
