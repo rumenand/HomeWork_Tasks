304f8fd1763057763f7e6930026eaac7b7d4008c
rumenand <rumen80@abv.bg>
2019-09-14 15:12:32 +0300

    Task 4 added on 100%

--- CSharp Advanced/03.Lab Sets and Dictionaries Advanced/4. Cities by Continent and Country/Program.cs
+++ CSharp Advanced/03.Lab Sets and Dictionaries Advanced/4. Cities by Continent and Country/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _4._Cities_by_Continent_and_Country
 {
@@ -6,7 +7,35 @@ namespace _4._Cities_by_Continent_and_Country
     {
         static void Main()
         {
-
+            var continentsData = new Dictionary<string, Dictionary<string, List<string>>>();
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine().Split();
+                var continent = tokens[0];
+                var country = tokens[1];
+                var city = tokens[2];
+                if (!continentsData.ContainsKey(continent))
+                {
+                    continentsData[continent] = new Dictionary<string, List<string>>();
+                }
+                if (!continentsData[continent].ContainsKey(country))
+                {
+                    continentsData[continent][country] = new List<string>();
+                }
+                continentsData[continent][country].Add(city);
+            }
+            foreach (var continentCountries in continentsData)
+            {
+                var continentName = continentCountries.Key;
+                Console.WriteLine($"{continentName}:");
+                foreach (var countryCities in continentCountries.Value)
+                {
+                    var countryName = countryCities.Key;
+                    var cities = countryCities.Value;
+                    Console.WriteLine($"  {countryName} -> {string.Join(", ",cities)}");
+                }
+            }
         }
     }
 }
