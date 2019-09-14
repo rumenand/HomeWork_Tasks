69e40ff2c5ba97fc3192c10b5481e03cc652b948
rumenand <rumen80@abv.bg>
2019-09-14 15:21:49 +0300

    Task 5 done on 100%

--- CSharp Advanced/03.Lab Sets and Dictionaries Advanced/5. Record Unique Names/Program.cs
+++ CSharp Advanced/03.Lab Sets and Dictionaries Advanced/5. Record Unique Names/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _5._Record_Unique_Names
 {
@@ -6,7 +7,15 @@ namespace _5._Record_Unique_Names
     {
         static void Main()
         {
-            
+            var names = new HashSet<string>();
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var name = Console.ReadLine();
+                names.Add(name);
+            }
+            foreach (var name in names)
+                Console.WriteLine(name);
         }
     }
 }
