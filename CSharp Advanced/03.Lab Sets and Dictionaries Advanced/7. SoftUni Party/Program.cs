2388006ce5ea5f0b653b89cd5b7a8e233b41667a
rumenand <rumen80@abv.bg>
2019-09-14 15:44:16 +0300

    Task 7 done on 100%

--- CSharp Advanced/03.Lab Sets and Dictionaries Advanced/7. SoftUni Party/Program.cs
+++ CSharp Advanced/03.Lab Sets and Dictionaries Advanced/7. SoftUni Party/Program.cs
@@ -13,12 +13,20 @@ namespace _7._SoftUni_Party
             string input;
             while ((input = Console.ReadLine()) != "PARTY")
             {
-
+                if (input.Length == 8)
+                {
+                    if (char.IsDigit(input[0])) vipGuests.Add(input);
+                    else regGuests.Add(input);
+                }
             }
             while ((input = Console.ReadLine()) != "END")
             {
-
+                if (regGuests.Contains(input)) regGuests.Remove(input);
+                else if (vipGuests.Contains(input)) vipGuests.Remove(input);
             }
+            Console.WriteLine(regGuests.Count+vipGuests.Count);
+            foreach (var guest in vipGuests) Console.WriteLine(guest);
+            foreach (var guest in regGuests) Console.WriteLine(guest);
         }
     }
 }
