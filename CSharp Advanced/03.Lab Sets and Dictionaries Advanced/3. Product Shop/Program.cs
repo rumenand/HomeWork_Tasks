ed2cef51ab6052008a1df889fe2c49b796caa3c3
rumenand <rumen80@abv.bg>
2019-09-14 15:01:40 +0300

    Task 3 done on 100%.

--- CSharp Advanced/03.Lab Sets and Dictionaries Advanced/3. Product Shop/Program.cs
+++ CSharp Advanced/03.Lab Sets and Dictionaries Advanced/3. Product Shop/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _3._Product_Shop
 {
@@ -6,7 +8,30 @@ namespace _3._Product_Shop
     {
         static void Main()
         {
-            
+            var shops = new Dictionary<string, Dictionary<string, double>>();
+            string line;
+            while ((line = Console.ReadLine()) != "Revision")
+            {
+                string[] productsInfo = line.Split(", ");
+                string shop = productsInfo[0];
+                string product = productsInfo[1];
+                double price = double.Parse(productsInfo[2]);
+                if (!shops.ContainsKey(shop))
+                {
+                    shops.Add(shop, new Dictionary<string, double>());
+                }
+                shops[shop].Add(product, price);
+            }
+            var orderedShops = shops.OrderBy(s => s.Key).ToDictionary(x => x.Key, x => x.Value);
+            foreach (var shop in orderedShops)
+            {
+                Console.WriteLine($"{shop.Key}->");
+                foreach (var product in shop.Value)
+                {
+                    Console.Write($"Product: {product.Key}, Price: {product.Value}");
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
