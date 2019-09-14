61d2265e760382d6a1b4fcbbd40b0cfa8304ff8a
rumenand <rumen80@abv.bg>
2019-09-14 14:49:57 +0300

    Task 2 done on 100%

--- CSharp Advanced/03.Lab Sets and Dictionaries Advanced/2. Average Student Grades/Program.cs
+++ CSharp Advanced/03.Lab Sets and Dictionaries Advanced/2. Average Student Grades/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _2._Average_Student_Grades
 {
@@ -6,7 +8,27 @@ namespace _2._Average_Student_Grades
     {
         static void Main()
         {
-           
+            var grades = new Dictionary<string, List<double>>();
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine().Split();
+                var name = tokens[0];
+                var grade = double.Parse(tokens[1]);
+                if (!grades.ContainsKey(name))
+                    grades[name] = new List<double>();
+                grades[name].Add(grade);
+            }
+            foreach (var pair in grades)
+            {
+                var name = pair.Key;
+                var studentGrades = pair.Value;
+                var average = studentGrades.Average();
+                Console.Write($"{name} -> ");
+                foreach (var grade in studentGrades)
+                    Console.Write($"{grade:f2} ");
+                Console.WriteLine($"(avg: {average:f2})");
+            }
         }
     }
 }
