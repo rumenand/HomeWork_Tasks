5ba047abc250699dec5ca27ee566cafbb10c9b5a
rumenand <rumen80@abv.bg>
2019-09-15 18:08:14 +0300

    Task 3 added empty.

--- CSharp Advanced/03.Exercises Sets and Dictionaries Advanced/Problem 2. Sets of Elements/Program.cs
+++ CSharp Advanced/03.Exercises Sets and Dictionaries Advanced/Problem 2. Sets of Elements/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Problem_2._Sets_of_Elements
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int[] lengths = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var setN = new HashSet<int>();
+            var setM = new HashSet<int>();
+            for (int i=0; i<lengths[0]; i++)
+            {
+                setN.Add(int.Parse(Console.ReadLine()));
+            }
+            for (int i=0;i<lengths[1];i++)
+            {
+                setM.Add(int.Parse(Console.ReadLine()));
+            }
+            foreach (var num in setN)
+            {
+                if (setM.Contains(num)) Console.Write($"{num} ");
+            }
         }
     }
 }
---
+++ CSharp Advanced/03.Exercises Sets and Dictionaries Advanced/Problem 3. Periodic Table/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem_3._Periodic_Table
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
