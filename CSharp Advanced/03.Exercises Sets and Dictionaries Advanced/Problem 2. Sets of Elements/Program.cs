78184f57159bb2c817f3814a327d93e6729b953e
rumenand <rumen80@abv.bg>
2019-09-15 17:59:12 +0300

    Task 2 added empty

---
+++ CSharp Advanced/03.Exercises Sets and Dictionaries Advanced/Problem 2. Sets of Elements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem_2._Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
