28ff2c46bd3107eed5d475cdfbd2c72686c044cf
rumenand <rumen80@abv.bg>
2019-09-15 17:58:21 +0300

    Task 1 done on 100%

--- CSharp Advanced/03.Exercises Sets and Dictionaries Advanced/Problem 1. Unique Usernames/Program.cs
+++ CSharp Advanced/03.Exercises Sets and Dictionaries Advanced/Problem 1. Unique Usernames/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Problem_1._Unique_Usernames
 {
@@ -6,7 +7,14 @@ namespace Problem_1._Unique_Usernames
     {
         static void Main()
         {
-            
+            int N = int.Parse(Console.ReadLine());
+            var names = new HashSet<string>();
+            for (int i =0; i<N;i++)
+            {
+                names.Add(Console.ReadLine());
+            }
+            foreach (var name in names)
+                Console.WriteLine(name);
         }
     }
 }
