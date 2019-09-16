42f089c512cca85313b335f7ccb764cb5f99d153
rumenand <rumen80@abv.bg>
2019-09-16 12:09:35 +0300

    Task 8 added empty.

---
+++ CSharp Advanced/03.Exercises Sets and Dictionaries Advanced/Problem 8. Ranking/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_8._Ranking
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
