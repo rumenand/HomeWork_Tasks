77730bbab21aa1f7af98b85cb8868137b756c235
rumenand <rumen80@abv.bg>
2019-09-16 09:11:43 +0300

    task 5 done on 100%

--- CSharp Advanced/03.Exercises Sets and Dictionaries Advanced/Problem 5. Count Symbols/Program.cs
+++ CSharp Advanced/03.Exercises Sets and Dictionaries Advanced/Problem 5. Count Symbols/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Problem_5._Count_Symbols
 {
@@ -6,7 +8,15 @@ namespace Problem_5._Count_Symbols
     {
         static void Main()
         {
-            
+            var input = Console.ReadLine();
+            var characters = new Dictionary<char, int>();
+            foreach (var ch in input)
+            {
+                if (!characters.ContainsKey(ch)) characters.Add(ch, 1);
+                else characters[ch]++;
+            }
+            foreach (var item in characters.OrderBy(x=>x.Key))
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
         }
     }
 }
