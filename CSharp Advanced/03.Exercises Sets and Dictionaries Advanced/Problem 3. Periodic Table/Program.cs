40ea9dab38af0b03a4f14b56625007ace698ca7e
rumenand <rumen80@abv.bg>
2019-09-16 08:49:43 +0300

    Task 3 done on 100%

--- CSharp Advanced/03.Exercises Sets and Dictionaries Advanced/Problem 3. Periodic Table/Program.cs
+++ CSharp Advanced/03.Exercises Sets and Dictionaries Advanced/Problem 3. Periodic Table/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Problem_3._Periodic_Table
 {
@@ -6,7 +7,15 @@ namespace Problem_3._Periodic_Table
     {
         static void Main()
         {
-            
+            int N = int.Parse(Console.ReadLine());
+            var elements = new SortedSet<string>();
+            for (int i = 0; i < N; i++)
+            {
+                var names = Console.ReadLine().Split();
+                foreach (var name in names)
+                    elements.Add(name);
+            }
+            Console.WriteLine(string.Join(" ", elements));
         }
     }
 }
