4113589cc8c38f1e18be427c6cfb0e6c06d0fc4b
rumenand <rumen80@abv.bg>
2019-09-16 09:04:02 +0300

    Task 4 done on 100%

--- CSharp Advanced/03.Exercises Sets and Dictionaries Advanced/Problem 4. Even Times/Program.cs
+++ CSharp Advanced/03.Exercises Sets and Dictionaries Advanced/Problem 4. Even Times/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Problem_4._Even_Times
 {
@@ -6,7 +7,22 @@ namespace Problem_4._Even_Times
     {
         static void Main()
         {
-            
+            int N = int.Parse(Console.ReadLine());
+            var numbers = new Dictionary<int, int>();
+            for (int i=0;i<N;i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                if (!numbers.ContainsKey(currentNum)) numbers.Add(currentNum, 1);
+                else numbers[currentNum]++;                
+            }
+            foreach (var num in numbers)
+            {
+                if (num.Value % 2 == 0)
+                {
+                    Console.WriteLine(num.Key);
+                    break;
+                }
+            }
         }
     }
 }
