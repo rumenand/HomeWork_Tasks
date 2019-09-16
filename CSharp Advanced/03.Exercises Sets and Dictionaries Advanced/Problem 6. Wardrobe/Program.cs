21e278cf938ecb5256ebbae2ffcc153c6147de37
rumenand <rumen80@abv.bg>
2019-09-16 11:32:54 +0300

    Task6 done on 100%.

--- CSharp Advanced/03.Exercises Sets and Dictionaries Advanced/Problem 6. Wardrobe/Program.cs
+++ CSharp Advanced/03.Exercises Sets and Dictionaries Advanced/Problem 6. Wardrobe/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Problem_6._Wardrobe
 {
@@ -6,7 +7,36 @@ namespace Problem_6._Wardrobe
     {
         static void Main()
         {
-           
+            int N = int.Parse(Console.ReadLine());
+            var clothes = new Dictionary<string, Dictionary<string,int>>();
+            for (int i = 0; i < N; i++)
+            {
+                var currentCommand = Console.ReadLine().Split(" -> ");
+                string curColor = currentCommand[0];
+                if (!clothes.ContainsKey(curColor))                
+                    clothes.Add(curColor, new Dictionary<string, int>());
+                var curClothes = currentCommand[1].Split(",");
+                for (int j = 0; j < curClothes.Length; j++)
+                {
+                    string curCloth = curClothes[j];
+                    if (!clothes[curColor].ContainsKey(curCloth))
+                        clothes[curColor].Add(curCloth, 1);
+                    else clothes[curColor][curCloth]++;
+                }
+            }
+            string[] search = Console.ReadLine().Split();
+            foreach (var color in clothes)
+            {
+                Console.WriteLine($"{color.Key} clothes:");
+                foreach (var cloth in color.Value)
+                {
+                    Console.Write($"* {cloth.Key} - {cloth.Value}");
+                    if (search[0] == color.Key && search[1] == cloth.Key)
+                        Console.WriteLine($" (found!)");
+                    else Console.WriteLine();
+                }
+
+            }
         }
     }
 }
