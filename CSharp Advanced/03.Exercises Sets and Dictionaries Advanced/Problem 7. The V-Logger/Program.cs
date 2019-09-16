16f3ac2c8a1e9c942c21c3e516a0dd157e28ea73
rumenand <rumen80@abv.bg>
2019-09-16 11:34:12 +0300

    Task 7 added empty.

---
+++ CSharp Advanced/03.Exercises Sets and Dictionaries Advanced/Problem 7. The V-Logger/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_7._The_V_Logger
+{
+    class Program
+    {
+        static void Main()
+        {
+        
+        }
+    }
+}
