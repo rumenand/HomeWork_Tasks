3ddf97e9203c7b74e0bef693eb1d432a453415cd
rumenand <rumen80@abv.bg>
2019-09-18 20:33:55 +0300

    Task 3 done on 100%

--- CSharp Advanced/05. Lab Functional Programming/3. Count Uppercase Words/Program.cs
+++ CSharp Advanced/05. Lab Functional Programming/3. Count Uppercase Words/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _3._Count_Uppercase_Words
 {
@@ -6,7 +7,15 @@ namespace _3._Count_Uppercase_Words
     {
         static void Main()
         {
-            
+            Func<string, bool> checker = n => n[0] == n.ToUpper()[0];
+            var words = Console.ReadLine().Split(new string[] { " " },
+            StringSplitOptions.RemoveEmptyEntries)
+            .Where(checker)
+            .ToArray();
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
         }
     }
 }
