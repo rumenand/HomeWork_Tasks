7aa8d7f41907a224e37dfb7e0036c521bfb2c3ea
rumenand <rumen80@abv.bg>
2019-09-18 21:09:37 +0300

    Task 5 done on 100%

--- CSharp Advanced/05. Lab Functional Programming/5. Filter by Age/Program.cs
+++ CSharp Advanced/05. Lab Functional Programming/5. Filter by Age/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _5._Filter_by_Age
 {
@@ -6,7 +8,54 @@ namespace _5._Filter_by_Age
     {
         static void Main()
         {
-            
+            int N = int.Parse(Console.ReadLine());
+            var people = new Dictionary<string, int>();
+            for (int i=0; i<N; i++)
+            {
+                string[] input = Console.ReadLine().Split(", ");
+                people.Add(input[0], int.Parse(input[1]));
+            }
+            string condition = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string format = Console.ReadLine();
+            Func<int, bool> tester = CreateTester(condition, age);
+            Action<KeyValuePair<string, int>> printer =
+            CreatePrinter(format);
+            PrintFilteredStudent(people, tester, printer);
+        }
+
+        public static Func<int, bool> CreateTester
+                    (string condition, int age)
+        {
+            switch (condition)
+            {
+                case "younger": return x => x < age;
+                case "older": return x => x >= age;
+                default: return null;
+            }
+        }
+
+        public static Action<KeyValuePair<string, int>>
+                        CreatePrinter(string format)
+        {
+            switch (format)
+            {
+                case "name":
+                    return pe=> Console.WriteLine($"{pe.Key}");                    
+                case "name age":
+                    return person => Console.WriteLine($"{person.Key} - {person.Value}");
+                case "age":
+                    return person => Console.WriteLine($"{person.Value}");
+                default: return null;
+            }
+        }
+
+        public static void PrintFilteredStudent(Dictionary<string,int> people, Func<int, bool> tester, Action<KeyValuePair<string, int>> printer)
+        {
+            foreach (var person in people)
+            {
+                if (tester(person.Value)) printer(person);
+            }
         }
     }
 }
