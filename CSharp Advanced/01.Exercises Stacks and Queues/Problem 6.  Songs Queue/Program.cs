e89396e84933f9d17a741734b5818093c1e2045b
rumenand <rumen80@abv.bg>
2019-09-18 10:32:57 +0300

    Task 6 done on 100%

--- CSharp Advanced/01.Exercises Stacks and Queues/Problem 6.  Songs Queue/Program.cs
+++ CSharp Advanced/01.Exercises Stacks and Queues/Problem 6.  Songs Queue/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Problem_6.__Songs_Queue
 {
@@ -6,7 +7,31 @@ namespace Problem_6.__Songs_Queue
     {
         static void Main()
         {
-           
+            Queue<string> songs = new Queue<string>(Console.ReadLine().Split(", "));
+            while (songs.Count>0)
+            {
+                string [] command = Console.ReadLine().Split();
+                switch (command[0])
+                {
+                    case "Play":
+                        songs.Dequeue();
+                        break;
+                    case "Add":
+                        List<string> songWords = new List<string>();
+                        for (int i=1; i<command.Length; i++)
+                        {
+                            songWords.Add(command[i]);
+                        }
+                        string song = string.Join(" ", songWords);
+                        if (!songs.Contains(song))  songs.Enqueue(song);
+                        else Console.WriteLine($"{song} is already contained!");
+                        break;
+                    case "Show":
+                        Console.WriteLine(string.Join(", ", songs));
+                        break;                       
+                }
+            }
+            Console.WriteLine("No more songs!");
         }
     }
 }
