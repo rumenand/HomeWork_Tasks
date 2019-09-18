2c733b6d86e6baa34ab9bd8a896140c2d7382c89
rumenand <rumen80@abv.bg>
2019-09-18 20:37:29 +0300

    Task 4 don e on 100%

--- CSharp Advanced/05. Lab Functional Programming/4. Add VAT/Program.cs
+++ CSharp Advanced/05. Lab Functional Programming/4. Add VAT/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _4._Add_VAT
 {
@@ -6,7 +7,14 @@ namespace _4._Add_VAT
     {
         static void Main()
         {
-            
+            double[] prices = Console.ReadLine()
+            .Split(new string[] { ", " },
+            StringSplitOptions.RemoveEmptyEntries)
+            .Select(double.Parse)
+            .Select(n => n * 1.2)
+            .ToArray();
+            foreach (var price in prices)
+                Console.WriteLine($"{price:f2}");
         }
     }
 }
