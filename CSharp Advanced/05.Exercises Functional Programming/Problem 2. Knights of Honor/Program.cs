e87262ac007ccb94639b69b67b32ecd1ff2d5efd
rumenand <rumen80@abv.bg>
2019-09-19 09:22:20 +0300

    Task 3 done on 100%

--- CSharp Advanced/05.Exercises Functional Programming/Problem 2. Knights of Honor/Program.cs
+++ CSharp Advanced/05.Exercises Functional Programming/Problem 2. Knights of Honor/Program.cs
@@ -6,7 +6,10 @@ namespace Problem_2._Knights_of_Honor
     {
         static void Main()
         {
-            
+            Action<string> printSir = name => Console.WriteLine($"Sir {name}");
+            string[] input = Console.ReadLine().Split();
+            foreach (var name in input)
+                printSir(name);
         }
     }
 }
