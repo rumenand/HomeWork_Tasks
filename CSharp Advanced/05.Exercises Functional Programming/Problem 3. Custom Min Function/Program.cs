49806210ec7a5ae6ab9bfe8218b5ff081bc70e95
rumenand <rumen80@abv.bg>
2019-09-19 09:41:44 +0300

    Task 3 added initial logic.

--- CSharp Advanced/05.Exercises Functional Programming/Problem 3. Custom Min Function/Program.cs
+++ CSharp Advanced/05.Exercises Functional Programming/Problem 3. Custom Min Function/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Problem_3._Custom_Min_Function
 {
@@ -6,7 +7,14 @@ namespace Problem_3._Custom_Min_Function
     {
         static void Main()
         {
-            
+            var numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Func<string [], int> GetMin = CalcMinNumber;
+            Console.WriteLine(GetMin(numbers));
+        }
+
+        static int CalcMinNumber(string[] numbers)
+        {
+            return 0;
         }
     }
 }
