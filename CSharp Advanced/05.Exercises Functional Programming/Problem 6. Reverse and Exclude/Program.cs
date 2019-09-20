e2b379b08f881552d16f9681433dd89d696e414a
rumenand <rumen80@abv.bg>
2019-09-20 22:17:46 +0300

    Task 6 done 100%

--- CSharp Advanced/05.Exercises Functional Programming/Problem 6. Reverse and Exclude/Program.cs
+++ CSharp Advanced/05.Exercises Functional Programming/Problem 6. Reverse and Exclude/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Problem_6._Reverse_and_Exclude
 {
@@ -6,7 +7,11 @@ namespace Problem_6._Reverse_and_Exclude
     {
         static void Main()
         {
-            
+            var nums = Console.ReadLine();
+            int divider = int.Parse(Console.ReadLine());          
+            Func<int, bool> checker = n => n % divider != 0;
+            var sortedNums = nums.Split().Select(int.Parse).Reverse().Where(checker);
+            Console.WriteLine(string.Join(" ", sortedNums));   
         }
     }
 }
