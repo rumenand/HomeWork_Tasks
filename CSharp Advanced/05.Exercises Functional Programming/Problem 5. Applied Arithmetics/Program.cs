97ed8512b027b60e3d10de32fb3eb137a2e731af
rumenand <rumen80@abv.bg>
2019-09-20 09:24:31 +0300

    Task 5 done 100%

--- CSharp Advanced/05.Exercises Functional Programming/Problem 5. Applied Arithmetics/Program.cs
+++ CSharp Advanced/05.Exercises Functional Programming/Problem 5. Applied Arithmetics/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Problem_5._Applied_Arithmetics
 {
@@ -6,7 +8,30 @@ namespace Problem_5._Applied_Arithmetics
     {
         static void Main()
         {
-            
+            Func<int [] , int[]> addOne = n => n.Select(x => x +1).ToArray();
+            Func<int[], int[]> multiplyTwo = n => n.Select(x => x * 2).ToArray();
+            Func<int[], int[]> substractOne = n => n.Select(x => x - 1).ToArray();
+            Action<int[]> print = col => Console.WriteLine(string.Join(" ",col)); 
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string input;
+            while((input = Console.ReadLine()) != "end")
+            {
+                switch (input)
+                {
+                    case "add":
+                         nums = addOne(nums);
+                        break;
+                    case "multiply":
+                        nums = multiplyTwo(nums);
+                        break;
+                    case "subtract":
+                        nums = substractOne(nums);
+                        break;
+                    case "print":
+                        print(nums);
+                        break;
+                }
+            }
         }
     }
 }
