fd3b77ae3f2dda96896e1aef1c51f05b98844174
rumenand <rumen80@abv.bg>
2019-09-20 23:03:37 +0300

    Task 8 done 100%

---
+++ CSharp Advanced/05.Exercises Functional Programming/Problem 8. Custom Comparator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Problem_8._Custom_Comparator
+{
+    public class CustomComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            if (x % 2 == 0 && y % 2 != 0) return -1;
+            else if (x % 2 != 0 && y % 2 == 0) return 1;
+            else return x.CompareTo(y);            
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            CustomComparer comp = new CustomComparer();
+            Array.Sort(nums, comp);
+            Console.WriteLine(string.Join(" ",nums));
+        }
+    }
+}
