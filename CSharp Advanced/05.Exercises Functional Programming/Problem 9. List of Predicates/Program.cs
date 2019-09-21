6d9ea62b8121dd29aa40fd485923769595b13640
rumenand <rumen80@abv.bg>
2019-09-21 22:31:49 +0300

    Task 9 done on 75%

--- CSharp Advanced/05.Exercises Functional Programming/Problem 9. List of Predicates/Program.cs
+++ CSharp Advanced/05.Exercises Functional Programming/Problem 9. List of Predicates/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Problem_9._List_of_Predicates
 {
@@ -6,7 +7,16 @@ namespace Problem_9._List_of_Predicates
     {
         static void Main()
         {
-            
+            int N = int.Parse(Console.ReadLine());
+            var numbers = new int[N];
+            var dividers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Func<int, bool> checker = n => dividers.All(x=>n%x ==0);
+            for (int i=0;i<N;i++)
+            {
+                numbers[i] = i + 1;
+            }
+            var result = numbers.Where(checker);
+            Console.WriteLine(string.Join(" ",result));
         }
     }
 }
