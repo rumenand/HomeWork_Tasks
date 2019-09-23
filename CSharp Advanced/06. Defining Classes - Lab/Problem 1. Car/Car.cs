f3db7325b673bb91b5942c8246fb4114ebf12f7b
rumenand <rumen80@abv.bg>
2019-09-23 22:05:38 +0300

    Added new project -> Defining Classes - Lab. Task are done on .NET Framework because I have .Net Core SDK ver 2.2 which is not recognized from judge.

---
+++ CSharp Advanced/06. Defining Classes - Lab/Problem 1. Car/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+    }
+}
