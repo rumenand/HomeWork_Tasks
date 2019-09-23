0f08b00f153caceda30cc7fdc7e79aac41b62b6b
rumenand <rumen80@abv.bg>
2019-09-23 22:49:06 +0300

    Task 2 done 100%

---
+++ CSharp Advanced/06. Defining Classes - Lab/Problem 2. Car Extension/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Car car = new Car();
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 200;
+            car.Drive(200);
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
