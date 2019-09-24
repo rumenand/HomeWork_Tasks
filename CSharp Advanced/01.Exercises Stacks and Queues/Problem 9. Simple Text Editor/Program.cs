2523e3cf5d5585bc2bc689ff1b54875094e44c02
rumenand <rumen80@abv.bg>
2019-09-24 22:43:41 +0300

    Task 9 improved logic

--- CSharp Advanced/01.Exercises Stacks and Queues/Problem 9. Simple Text Editor/Program.cs
+++ CSharp Advanced/01.Exercises Stacks and Queues/Problem 9. Simple Text Editor/Program.cs
@@ -14,31 +14,31 @@ namespace Problem_9._Simple_Text_Editor
             for (int i=0; i<lines;i++)
             {
                 string[] commands = Console.ReadLine().Split();
-                switch (commands[0])
-                {
-                    case "1":
-                        sb.Append(commands[1]);
-                        undoStack.Push("2 " + commands[1].Length);
-                        break;
-                    case "2":
-                        int subLength = int.Parse(commands[1]);
-                        string substract = sb.ToString().Substring(sb.Length - subLength);
-                        sb.Remove(sb.Length - subLength, subLength);
-                        undoStack.Push("1 " + substract);
-                        break;
-                    case "3":
-                        Console.WriteLine(sb[int.Parse(commands[1])-1]);
-                        break;
-                    case "4":
-                        string [] undo = undoStack.Pop().Split();
-                        if (undo[0] == "1") sb.Append(undo[1]);
-                        else if (undo[0] == "2")
-                        {
-                            int subL = int.Parse(undo[1]);
-                            sb.Remove(sb.Length - subL, subL);
-                        }
-                        break;
-                }
+                ExecuteCommand(sb, commands, undoStack, true);
+            }            
+        }
+
+        static void ExecuteCommand (StringBuilder sb, string [] commands, Stack<string> undoStack,  bool undo)
+        {            
+            switch (commands[0])
+            {
+                case "1":
+                    sb.Append(commands[1]);
+                    if (undo) undoStack.Push("2 " + commands[1].Length);
+                    break;
+                case "2":
+                    int subLength = int.Parse(commands[1]);
+                    string substract = sb.ToString().Substring(sb.Length - subLength);
+                    sb.Remove(sb.Length - subLength, subLength);
+                    if (undo)  undoStack.Push("1 " + substract);
+                    break;
+                case "3":
+                    Console.WriteLine(sb[int.Parse(commands[1]) - 1]);
+                    break;
+                case "4":
+                    string[] undoCommand = undoStack.Pop().Split();
+                    ExecuteCommand(sb, undoCommand, undoStack, false);
+                    break;
             }
         }
     }
