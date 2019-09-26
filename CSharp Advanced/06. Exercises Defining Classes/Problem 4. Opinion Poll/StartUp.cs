3d2f7c7dfb71b18a7745db3a8bfb8d2b63418ce6
rumenand <rumen80@abv.bg>
2019-09-26 21:35:18 +0300

    Task 4 done on 0 tests

--- CSharp Advanced/06. Exercises Defining Classes/Problem 4. Opinion Poll/StartUp.cs
+++ CSharp Advanced/06. Exercises Defining Classes/Problem 4. Opinion Poll/StartUp.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DefiningClasses
 {
@@ -6,7 +8,18 @@ namespace DefiningClasses
     {
         static void Main()
         {
-
+            List<Person> pepole = new List<Person>();
+            int N = int.Parse(Console.ReadLine());          
+            for (int i = 0; i < N; i++)
+            {
+                var person = Console.ReadLine().Split();
+                pepole.Add(new Person(person[0], int.Parse(person[1])));
+            }
+            var sortedPeople = pepole.Where(x => x.Age > 30).OrderBy(x => x.Name);
+            foreach (var person in sortedPeople)
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
         }
     }
 }
