aec24bc084fee4e6e62940a15666fd087d07ae9f
rumenand <rumen80@abv.bg>
2019-09-26 21:25:31 +0300

    Task 3 done on 0 tests

---
+++ CSharp Advanced/06. Exercises Defining Classes/Problem 3. Oldest Family Member/Family.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> listOfpPepople;
+
+        public Family()
+        {
+            this.listOfpPepople = new List<Person>();
+        }
+
+        public void AddMember (Person member)
+        {
+            listOfpPepople.Add(member);
+        }
+
+        public Person GetOldestMember ()
+        {
+            return listOfpPepople.OrderByDescending(x => x.Age).First();
+        }
+    }
+}
--- CSharp Advanced/06. Exercises Defining Classes/Problem 3. Oldest Family Member/StartUp.cs
+++ CSharp Advanced/06. Exercises Defining Classes/Problem 3. Oldest Family Member/StartUp.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace DefiningClasses
 {
@@ -6,7 +7,15 @@ namespace DefiningClasses
     {
         static void Main()
         {
-
+            int N = int.Parse(Console.ReadLine());
+            Family family = new Family();
+            for (int i=0; i<N;i++)
+            {
+                var person = Console.ReadLine().Split();
+                family.AddMember(new Person(person[0], int.Parse(person[1])));
+            }
+            var oldestPerson = family.GetOldestMember();
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
         }
     }
 }
