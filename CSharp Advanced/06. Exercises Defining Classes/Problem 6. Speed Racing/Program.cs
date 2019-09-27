3ffb8b56235c84b629da71f0b854b99f47fec7a1
rumenand <rumen80@abv.bg>
2019-09-27 23:47:35 +0300

    Task 6 done on 0 tests

--- CSharp Advanced/06. Exercises Defining Classes/Problem 6. Speed Racing/Program.cs
+++ CSharp Advanced/06. Exercises Defining Classes/Problem 6. Speed Racing/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Problem_6._Speed_Racing
 {
@@ -6,7 +7,27 @@ namespace Problem_6._Speed_Racing
     {
         static void Main()
         {
-            
+            var carsList = new Dictionary<string,Car>();
+            var lines = int.Parse(Console.ReadLine());
+            for (int i= 0; i<lines;i++)
+            {
+                var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var newCar = new Car();
+                newCar.Model = input[0];
+                newCar.FuelAmount = double.Parse(input[1]);
+                newCar.FuelConsumptionPerKilometer = double.Parse(input[2]);
+                carsList.Add(newCar.Model,newCar);
+            }
+            string line;
+            while((line = Console.ReadLine()) != "End")
+            {
+                var commands = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                if (!carsList[commands[1]].Drive(int.Parse(commands[2]))) Console.WriteLine("Insufficient fuel for the drive");
+            }
+            foreach (var car in carsList)
+            {
+                Console.WriteLine($"{car.Key} {car.Value.FuelAmount:F2} {car.Value.TravelledDistance}");
+            }
         }
     }
 }
