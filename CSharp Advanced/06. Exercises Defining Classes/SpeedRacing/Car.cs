1d04f07788d8c2556ec2e0d5ecf849ad3d37cc00
rumenand <rumen80@abv.bg>
2019-09-28 19:52:33 +0300

    Task 6 working on 40%

---
+++ CSharp Advanced/06. Exercises Defining Classes/SpeedRacing/Car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpeedRacing
+{
+    class Car
+    {
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumptionPerKilometer;
+        private double travelledDistance;
+
+        public double TravelledDistance
+        {
+            get { return travelledDistance; }
+            set { travelledDistance = value; }
+        }
+
+
+        public double FuelConsumptionPerKilometer
+        {
+            get { return fuelConsumptionPerKilometer; }
+            set { fuelConsumptionPerKilometer = value; }
+        }
+        public double FuelAmount
+        {
+            get { return fuelAmount; }
+            set { fuelAmount = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public bool Drive(int distance)
+        {
+            var neededFuel = distance * FuelConsumptionPerKilometer;
+            if (fuelAmount > neededFuel)
+            {
+                fuelAmount -= neededFuel;
+                travelledDistance += distance;
+                return true;
+            }
+            return false;
+        }
+    }
+}
