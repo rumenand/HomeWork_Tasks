c069c81c5dca66a31d5b118bb25e908b8e84c97d
rumenand <rumen80@abv.bg>
2019-09-28 17:51:13 +0300

    Task 3 working on judje

---
+++ CSharp Advanced/06. Exercises Defining Classes/OldestFamilyMember/Family.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OldestFamilyMember
+{
+    class Family
+    {
+        private List<Person> listOfpPepople;
+        public Family()
+        {
+            this.listOfpPepople = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            listOfpPepople.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return listOfpPepople.OrderByDescending(x => x.Age).First();
+        }
+    }
+}
---
+++ CSharp Advanced/06. Exercises Defining Classes/OldestFamilyMember/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OldestFamilyMember
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        { 
+            int N = int.Parse(Console.ReadLine());
+            Family family = new Family();
+            for (int i = 0; i < N; i++)
+            {
+                var person = Console.ReadLine().Split();
+                family.AddMember(new Person(person[0], int.Parse(person[1])));
+            }
+            var oldestPerson = family.GetOldestMember();
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+        }
+    }
+}
