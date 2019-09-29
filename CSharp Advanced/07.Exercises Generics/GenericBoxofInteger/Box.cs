8bc8dddb04eb80c9d48a150a1dcf5f2f5e3844a8
rumenand <rumen80@abv.bg>
2019-09-29 17:36:47 +0300

    Task 2 done on 100%

---
+++ CSharp Advanced/07.Exercises Generics/GenericBoxofInteger/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxofInteger
+{
+    public class Box<T>
+    {
+        private T Value;
+
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value.GetType().FullName}: {Value.ToString()}";
+        }
+    }
+}
---
+++ CSharp Advanced/07.Exercises Generics/GenericBoxofInteger/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GenericBoxofInteger
+{
+    class Program
+    {
+        static void Main()
+        {
+            var lines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lines; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+                var newBox = new Box<int>(input);
+                Console.WriteLine(newBox.ToString());
+            }
+        }
+    }
+}
