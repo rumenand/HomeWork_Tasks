6edc10621233b896be3b1881ede83081ef11fa09
rumenand <rumen80@abv.bg>
2019-09-29 21:03:23 +0300

    Task 5 done one 100%

---
+++ CSharp Advanced/07.Exercises Generics/GenericCountMethodStrings/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericCountMethodStrings
+{
+    public class Box<T> : IComparable<T>
+    where T : IComparable<T>
+    {
+        public T item;
+
+        public Box(T item)
+        {
+            this.item = item;
+        }
+        public int CompareTo(T other)
+        {
+            return this.item.CompareTo(other);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.item.GetType().FullName}: {this.item}";
+        }
+    }
+}
--- CSharp Advanced/07.Exercises Generics/GenericCountMethodStrings/Program.cs
+++ CSharp Advanced/07.Exercises Generics/GenericCountMethodStrings/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GenericCountMethodStrings
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            
+            var lines = int.Parse(Console.ReadLine());
+            var list = new List<Box<string>>();
+            for (int i = 0; i < lines; i++)
+            {
+                var input = Console.ReadLine();
+                list.Add(new Box<string>(input));
+            }
+            var value = Console.ReadLine();
+            Console.WriteLine(GetBiggerItems(list,value));           
+        }
+        static int GetBiggerItems<T> (IEnumerable<Box<T>> list, T compareItem) where T : IComparable<T>
+        {
+            int counter = 0;
+            foreach (var item in list)
+            {
+                if (item.CompareTo(compareItem) > 0) counter++;
+            }
+            return counter;
         }
     }
 }
