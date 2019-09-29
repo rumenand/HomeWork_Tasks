c793397da94c9446034311a98c1ea5daef4abe6e
rumenand <rumen80@abv.bg>
2019-09-29 14:12:37 +0300

    Added Lab Generics

---
+++ CSharp Advanced/07.Lab Generics/BoxOfT/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BoxOfT
+{
+    class Box<T>
+    {
+        private List<T> data;
+        public Box()
+        {
+            this.data = new List<T>();
+        }
+        public int Count => this.data.Count;
+        public void Add(T item) { this.data.Add(item); }
+        public T Remove()
+        {
+            var rem = this.data.Last();
+            this.data.RemoveAt(this.data.Count - 1);
+            return rem;
+        }
+    }
+}
---
+++ CSharp Advanced/07.Lab Generics/BoxOfT/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BoxOfT
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
