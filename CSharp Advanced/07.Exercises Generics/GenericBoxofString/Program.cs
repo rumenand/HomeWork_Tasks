e866b46a8dcfc6c8ac9ce1afed09b53c364f1e31
rumenand <rumen80@abv.bg>
2019-09-29 17:31:53 +0300

    Task 1 done on 100%

--- CSharp Advanced/07.Exercises Generics/GenericBoxofString/Program.cs
+++ CSharp Advanced/07.Exercises Generics/GenericBoxofString/Program.cs
@@ -6,7 +6,13 @@ namespace GenericBoxofString
     {
         static void Main()
         {
-            
+            var lines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lines; i++)
+            {
+                var input = Console.ReadLine();
+                var newBox = new Box<string>(input);
+                Console.WriteLine(newBox.ToString());
+            }
         }
     }
 }
