12a09ac845b66ef4242e6a211883c75bb7850bf6
rumenand <rumen80@abv.bg>
2019-09-29 19:43:21 +0300

    Task 3 done 100%

---
+++ CSharp Advanced/07.Exercises Generics/GenericSwapMethodStrings/Box.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericSwapMethodStrings
+{
+    public class Box<T>
+    {
+        public List<T> list;            
+        public Box()
+        {
+            this.list = new List<T>();
+        }
+
+        public void SwapElelments(int ind1, int ind2)
+        {
+            var temp = list[ind2];
+            list[ind2] = list[ind1];
+            list[ind1] = temp;
+            printList();
+        }
+
+        private void printList()
+        {
+            foreach (var value in list)
+            {
+                Console.WriteLine($"{value.GetType().FullName}: {value}");
+            }
+            
+        }
+    }
+}
--- CSharp Advanced/07.Exercises Generics/GenericSwapMethodStrings/Program.cs
+++ CSharp Advanced/07.Exercises Generics/GenericSwapMethodStrings/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace GenericSwapMethodStrings
 {
@@ -6,7 +7,15 @@ namespace GenericSwapMethodStrings
     {
         static void Main()
         {
-
+            var lines = int.Parse(Console.ReadLine());
+            var list = new Box<string>();
+            for (int i = 0; i < lines; i++)
+            {
+                var input = Console.ReadLine();
+                list.list.Add(input);
+            }
+            var swapIndexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            list.SwapElelments(swapIndexes[0],swapIndexes[1]);
         }
     }
 }
