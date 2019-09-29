d83164b62a5027511f7f223416c9b9f576565a7c
rumenand <rumen80@abv.bg>
2019-09-29 15:29:25 +0300

    Task 3 done on 100%

---
+++ CSharp Advanced/07.Lab Generics/GenericScale/EqualityScale.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        private T left;
+        private T right;
+
+        public EqualityScale(T leftValue, T rightValue)
+        {
+            this.left = leftValue;
+            this.right = rightValue;
+        }
+
+        public bool AreEqual()
+        {
+            bool result = this.left.Equals(this.right);
+            return result;
+        }
+    }
+}
