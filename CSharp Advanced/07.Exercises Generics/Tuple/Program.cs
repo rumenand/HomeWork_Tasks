2b25c68b91b625882872b312b717ac78346d60ba
rumenand <rumen80@abv.bg>
2019-09-30 23:04:32 +0300

    Task 7 done 100%

--- CSharp Advanced/07.Exercises Generics/Tuple/Program.cs
+++ CSharp Advanced/07.Exercises Generics/Tuple/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Tuple
+namespace Tuples
 {
     class Program
     {
         static void Main()
         {
-            
+            var input = Console.ReadLine().Split();
+            var tuple1 = new Tuple<string, string>(input[0] + " " + input[1], input[2]);
+            input = Console.ReadLine().Split();
+            var tuple2 = new Tuple<string, int>(input[0], int.Parse(input[1]));
+            input = Console.ReadLine().Split();
+            var tuple3 = new Tuple<int, double>(int.Parse(input[0]), double.Parse(input[1]));
+            Console.WriteLine(tuple1);
+            Console.WriteLine(tuple2);
+            Console.WriteLine(tuple3);
         }
     }
 }
---
+++ CSharp Advanced/07.Exercises Generics/Tuple/Tuple.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Tuples
+{
+    public class Tuple<TKey,TValue>
+    {
+        public TKey Item1 { get; set; }
+        public TValue Item2 { get; set; }
+
+        public Tuple(TKey item1, TValue item2)
+        {
+            this.Item1 = item1;
+            this.Item2 = item2;
+        }
+        public override string ToString()
+        {            
+            return $"{Item1} -> {Item2}";
+        }
+    }
+}
