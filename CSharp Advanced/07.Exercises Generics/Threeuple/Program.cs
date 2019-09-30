d6e3bae80b924c3d364f36c124dbe99389bc86fa
rumenand <rumen80@abv.bg>
2019-09-30 23:16:30 +0300

    Task 8 done 100%

--- CSharp Advanced/07.Exercises Generics/Threeuple/Program.cs
+++ CSharp Advanced/07.Exercises Generics/Threeuple/Program.cs
@@ -6,7 +6,15 @@ namespace Threeuple
     {
         static void Main(string[] args)
         {
-            
+            var input = Console.ReadLine().Split();
+            var tuple1 = new Tuple<string, string, string>(input[0] + " " + input[1], input[2], input[3]);
+            input = Console.ReadLine().Split();
+            var tuple2 = new Tuple<string, int, bool>(input[0], int.Parse(input[1]), (input[2] == "drunk" ?true:false));
+            input = Console.ReadLine().Split();
+            var tuple3 = new Tuple<string, double, string>(input[0], double.Parse(input[1]), input[2]);
+            Console.WriteLine(tuple1);
+            Console.WriteLine(tuple2);
+            Console.WriteLine(tuple3);
         }
     }
 }
---
+++ CSharp Advanced/07.Exercises Generics/Threeuple/Tuple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Threeuple
+{
+    class Tuple<TKey,TValue1,TValue2>
+    {
+        public TKey Item1 { get; set; }
+        public TValue1 Item2 { get; set; }
+        public TValue2 Item3 { get; set; }
+        public Tuple(TKey item1, TValue1 item2, TValue2 item3)
+        {
+            this.Item1 = item1;
+            this.Item2 = item2;
+            this.Item3 = item3;
+        }
+        public override string ToString()
+        {
+            return $"{Item1} -> {Item2} -> {Item3}";
+        }
+
+
+    }
+}
