78012f696ad687c034a1f5781f792a42fac8e31c
rumenand <rumen80@abv.bg>
2019-09-30 19:33:39 +0300

    Task 6 done on 100%

--- CSharp Advanced/07.Exercises Generics/GenericCountMethodDoubles/Program.cs
+++ CSharp Advanced/07.Exercises Generics/GenericCountMethodDoubles/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GenericCountMethodDoubles
 {
@@ -6,7 +7,25 @@ namespace GenericCountMethodDoubles
     {
         static void Main()
         {
-            
+            var lines = int.Parse(Console.ReadLine());
+            var list = new List<Box<double>>();
+            for (int i = 0; i < lines; i++)
+            {
+                var input = Console.ReadLine();
+                list.Add(new Box<double>(double.Parse(input)));
+            }
+            var value = double.Parse(Console.ReadLine());
+            Console.WriteLine(GetBiggerItems(list, value));
         }
+        static int GetBiggerItems<T>(IEnumerable<Box<T>> list, T compareItem) where T : IComparable<T>
+        {
+            int counter = 0;
+            foreach (var item in list)
+            {
+                if (item.CompareTo(compareItem) > 0) counter++;
+            }
+            return counter;
+        }
+    
     }
 }
