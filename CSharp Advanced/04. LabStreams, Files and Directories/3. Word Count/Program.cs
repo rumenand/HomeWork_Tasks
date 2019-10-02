f5ed673012eea29ce15651b88ad0932318d5db5c
rumenand <rumen80@abv.bg>
2019-10-02 23:07:17 +0300

    Task 3 completed

--- CSharp Advanced/04. LabStreams, Files and Directories/3. Word Count/Program.cs
+++ CSharp Advanced/04. LabStreams, Files and Directories/3. Word Count/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace _3._Word_Count
 {
@@ -8,13 +9,28 @@ namespace _3._Word_Count
     {
         static void Main()
         {
-            
-            var lines = File.ReadAllLines("words.txt");
-            for (var i = 0; i < lines.Length; i += 1) {
-             var line = lines[i];
-            // Process line
+            var wordsToCount = GetListFromFile("words.txt").ToDictionary(x=>x,y=>0);
+            var compareList = GetListFromFile("input.txt");
+           
+                foreach (var compWord in compareList)
+           {
+                    if (wordsToCount.ContainsKey(compWord)) wordsToCount[compWord]++;
             }
-            
+            foreach (var kvp in wordsToCount.OrderByDescending(x=>x.Value))
+                Console.WriteLine($"{kvp.Key} - {kvp.Value}");
+
+        }
+       
+        static List<string> GetListFromFile(string fileName)
+        {
+            var list = new List<string>();
+            var lines = File.ReadAllText(fileName).ToLower()
+                 .Split("\t\r\n\"'`/\\?!@#$%^&*+-_<>[]{}().,;: ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            foreach (var word in lines)
+            {
+                list.Add(word);
+            }
+            return list;
         }
     }
 }
