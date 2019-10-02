fde3dfd344b6c67be51b445a77cb04239bf2f313
rumenand <rumen80@abv.bg>
2019-10-02 19:46:00 +0300

    Added task 3 empty

--- CSharp Advanced/04. LabStreams, Files and Directories/3. Word Count/Program.cs
+++ CSharp Advanced/04. LabStreams, Files and Directories/3. Word Count/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace _3._Word_Count
 {
@@ -6,6 +8,12 @@ namespace _3._Word_Count
     {
         static void Main()
         {
+            
+            var lines = File.ReadAllLines("words.txt");
+            for (var i = 0; i < lines.Length; i += 1) {
+             var line = lines[i];
+            // Process line
+            }
             
         }
     }
