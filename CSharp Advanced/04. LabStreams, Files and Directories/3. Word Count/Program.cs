054e527bf092014da1de093c7fa3506fedb3a837
rumenand <rumen80@abv.bg>
2019-10-02 23:12:22 +0300

    Task 3 updated.

--- CSharp Advanced/04. LabStreams, Files and Directories/3. Word Count/Program.cs
+++ CSharp Advanced/04. LabStreams, Files and Directories/3. Word Count/Program.cs
@@ -9,28 +9,22 @@ namespace _3._Word_Count
     {
         static void Main()
         {
-            var wordsToCount = GetListFromFile("words.txt").ToDictionary(x=>x,y=>0);
-            var compareList = GetListFromFile("input.txt");
-           
-                foreach (var compWord in compareList)
+           var wordsToCount = GetListFromFile("words.txt").ToDictionary(x=>x,y=>0);
+           var compareList = GetListFromFile("input.txt");           
+           foreach (var compWord in compareList)
            {
-                    if (wordsToCount.ContainsKey(compWord)) wordsToCount[compWord]++;
-            }
-            foreach (var kvp in wordsToCount.OrderByDescending(x=>x.Value))
+             if (wordsToCount.ContainsKey(compWord)) wordsToCount[compWord]++;
+           }
+           foreach (var kvp in wordsToCount.OrderByDescending(x=>x.Value))
                 Console.WriteLine($"{kvp.Key} - {kvp.Value}");
-
         }
        
         static List<string> GetListFromFile(string fileName)
-        {
-            var list = new List<string>();
+        {            
             var lines = File.ReadAllText(fileName).ToLower()
-                 .Split("\t\r\n\"'`/\\?!@#$%^&*+-_<>[]{}().,;: ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-            foreach (var word in lines)
-            {
-                list.Add(word);
-            }
-            return list;
+                 .Split("\t\r\n\"'`/\\?!@#$%^&*+-_<>[]{}().,;: ".ToCharArray(),
+                 StringSplitOptions.RemoveEmptyEntries).ToList();           
+            return lines;
         }
     }
 }
