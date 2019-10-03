f89f3e490147bc3624b0ef84629bb9e29bb5129c
rumenand <rumen80@abv.bg>
2019-10-03 19:13:49 +0300

    Task 2 added empty

--- CSharp Advanced/04.Exercise Streams, Files and Directories/Problem 1. Even Lines/Program.cs
+++ CSharp Advanced/04.Exercise Streams, Files and Directories/Problem 1. Even Lines/Program.cs
@@ -9,7 +9,7 @@ namespace Problem_1._Even_Lines
         static void Main()
         {
             char[] separators = new char[] { '-', ',', '.', '!', '?' };
-            Func<string, string> replacer = x => separators.Aggregate(x, (c1, c2) => c1.Replace(c2, '@')); ;
+            Func<string, string> replacer = x => separators.Aggregate(x, (c1, c2) => c1.Replace(c2, '@'));
             StreamReader reader = new StreamReader("text.txt");
             using (reader)
             {
---
+++ CSharp Advanced/04.Exercise Streams, Files and Directories/Problem 2. Line Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem_2._Line_Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+          
+        }
+    }
+}
