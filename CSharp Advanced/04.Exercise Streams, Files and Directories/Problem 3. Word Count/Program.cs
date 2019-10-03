51f42c637728253416a7ead693b02e69e382c715
rumenand <rumen80@abv.bg>
2019-10-03 22:25:13 +0300

    Task 3 finished

--- CSharp Advanced/04.Exercise Streams, Files and Directories/Problem 3. Word Count/Program.cs
+++ CSharp Advanced/04.Exercise Streams, Files and Directories/Problem 3. Word Count/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Problem_3._Word_Count
 {
@@ -6,7 +9,29 @@ namespace Problem_3._Word_Count
     {
         static void Main()
         {
+            var words = File.ReadAllLines(@"words.txt").ToDictionary(x=>x,y=>0);
+            var text = File.ReadAllText("text.txt").ToLower()
+                .Split("\t\r\n\"'`/\\?!@#$%^&*+-_<>[]{}().,;: ".ToCharArray(),
+                StringSplitOptions.RemoveEmptyEntries).ToList();
+            foreach (var item in text)
+            {
+                if (words.ContainsKey(item)) words[item]++;
+            }
+            WriteTextFile("actualResult.txt", words);
+            WriteTextFile("expectedResult.txt", words.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, y => y.Value));
             
+        }
+
+        static void WriteTextFile(string fileName, Dictionary<string,int> words)
+        {
+            using (StreamWriter outputFile = new StreamWriter(fileName))
+            {
+                foreach (var item in words)
+                {
+                    outputFile.WriteLine($"{item.Key} - {item.Value}");
+                }
+            }
+
         }
     }
 }
