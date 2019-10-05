c05508541c654351a9276b844d2fde7306d2c07d
rumenand <rumen80@abv.bg>
2019-10-05 21:20:08 +0300

    Task 3 updated for Fil class

--- CSharp Advanced/04.Exercise Streams, Files and Directories/Problem 3. Word Count/Program.cs
+++ CSharp Advanced/04.Exercise Streams, Files and Directories/Problem 3. Word Count/Program.cs
@@ -9,21 +9,27 @@ namespace Problem_3._Word_Count
     {
         static void Main()
         {
-            var words = File.ReadAllLines(@"..\..\..\..\Resources\words.txt").ToDictionary(x=>x,y=>0);
-            var text = File.ReadAllText(@"..\..\..\..\Resources\text.txt").ToLower()
-                .Split("\t\r\n\"'`/\\?!@#$%^&*+-_<>[]{}().,;: ".ToCharArray(),
-                StringSplitOptions.RemoveEmptyEntries).ToList();
+            var words = File.ReadAllLines(@"..\..\..\..\Resources\words.txt")
+                        .ToDictionary(x=>x,y=>0);
+            var text = File.ReadAllText(@"..\..\..\..\Resources\text.txt")
+                        .ToLower()
+                        .Split("\t\r\n\"'`/\\?!@#$%^&*+-_<>[]{}().,;: ".ToCharArray(),
+                        StringSplitOptions.RemoveEmptyEntries).ToList();
+
             foreach (var item in text)
             {
                 if (words.ContainsKey(item)) words[item]++;
             }
-            //WriteTextFile(@"..\..\..\Output\output.txt", words);
-           WriteTextFile(@"..\..\..\Output\actualResult.txt", words.OrderByDescending(x => x.Value));            
-        }
+            
+           File.WriteAllLines(@"..\..\..\Output\actualResult.txt",
+               words.OrderByDescending(x => x.Value)
+               .Select(x => x.Key + " - " + x.Value)
+               .ToArray());
 
-        static void WriteTextFile(string fileName, IOrderedEnumerable<KeyValuePair<string, int>> words)
-        {
-         File.WriteAllLines(fileName,words.Select(x => x.Key + "-" + x.Value).ToArray());           
-        }
+            var areEquals = File.ReadLines(@"..\..\..\Output\actualResult.txt")
+                            .SequenceEqual
+                            (File.ReadLines(@"..\..\..\..\Resources\expectedResult.txt"));
+            Console.WriteLine(areEquals);
+        }        
     }
 }
