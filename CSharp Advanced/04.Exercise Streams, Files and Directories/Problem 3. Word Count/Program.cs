cb17f5814c90044ac0359e5075a9d58dc7b6cfce
rumenand <rumen80@abv.bg>
2019-10-05 21:06:41 +0300

    Task 3 adopted to work with external resourses

--- CSharp Advanced/04.Exercise Streams, Files and Directories/Problem 3. Word Count/Program.cs
+++ CSharp Advanced/04.Exercise Streams, Files and Directories/Problem 3. Word Count/Program.cs
@@ -9,29 +9,21 @@ namespace Problem_3._Word_Count
     {
         static void Main()
         {
-            var words = File.ReadAllLines(@"words.txt").ToDictionary(x=>x,y=>0);
-            var text = File.ReadAllText("text.txt").ToLower()
+            var words = File.ReadAllLines(@"..\..\..\..\Resources\words.txt").ToDictionary(x=>x,y=>0);
+            var text = File.ReadAllText(@"..\..\..\..\Resources\text.txt").ToLower()
                 .Split("\t\r\n\"'`/\\?!@#$%^&*+-_<>[]{}().,;: ".ToCharArray(),
                 StringSplitOptions.RemoveEmptyEntries).ToList();
             foreach (var item in text)
             {
                 if (words.ContainsKey(item)) words[item]++;
             }
-            WriteTextFile("actualResult.txt", words);
-            WriteTextFile("expectedResult.txt", words.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, y => y.Value));
-            
+            //WriteTextFile(@"..\..\..\Output\output.txt", words);
+           WriteTextFile(@"..\..\..\Output\actualResult.txt", words.OrderByDescending(x => x.Value));            
         }
 
-        static void WriteTextFile(string fileName, Dictionary<string,int> words)
+        static void WriteTextFile(string fileName, IOrderedEnumerable<KeyValuePair<string, int>> words)
         {
-            using (StreamWriter outputFile = new StreamWriter(fileName))
-            {
-                foreach (var item in words)
-                {
-                    outputFile.WriteLine($"{item.Key} - {item.Value}");
-                }
-            }
-
+         File.WriteAllLines(fileName,words.Select(x => x.Key + "-" + x.Value).ToArray());           
         }
     }
 }
