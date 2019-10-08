e5be7cbc228a34837104ddcbd90fbea9f1dc19ee
rumenand <rumen80@abv.bg>
2019-10-08 12:29:37 +0300

    Task 1 done 100%

---
+++ CSharp Advanced/09. CSharp-Advanced-Iterators-and-Comparators-Lab/1. Library/Library .cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class Library:IEnumerable<Book>
+    {
+        private List<Book> Books;
+        public Library(params Book [] books)
+        {
+            this.Books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return Books.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
