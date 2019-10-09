257eba4e7d21dd8fdeb8d46568c1af1941cacea5
rumenand <rumen80@abv.bg>
2019-10-09 06:44:10 +0300

    Task 4 done 100%

--- CSharp Advanced/09. CSharp-Advanced-Iterators-and-Comparators-Lab/4. Book Comparator/StartUp.cs
+++ CSharp Advanced/09. CSharp-Advanced-Iterators-and-Comparators-Lab/4. Book Comparator/StartUp.cs
@@ -6,7 +6,15 @@ namespace IteratorsAndComparators
     {
         static void Main()
         {
-            
+            Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+            Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
+            Book bookThree = new Book("The Documents in the Case", 1930);
+
+            Library library = new Library(bookOne, bookThree, bookTwo);
+            foreach (var book in library)
+            {
+                Console.WriteLine(book);
+            }
         }
     }
 }
