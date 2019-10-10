54c22a8c1bea7c6c1c59e3da80f0826046d1640e
rumenand <rumen80@abv.bg>
2019-10-10 07:52:21 +0300

    Task 2 done on 100%

--- CSharp Advanced/09.Exercises-Iterators And Comparators/Problem 2. Collection/Program.cs
+++ CSharp Advanced/09.Exercises-Iterators And Comparators/Problem 2. Collection/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace IteratorsAndComparators
 {
@@ -6,7 +7,36 @@ namespace IteratorsAndComparators
     {
         static void Main()
         {
-            
+            var listy = CreateListy();
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                switch (input)
+                {
+                    case "Move":
+                        Console.WriteLine(listy.Move());
+                        break;
+                    case "Print":
+                        listy.Print();
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(listy.HasNext());
+                        break;
+                    case "PrintAll":
+                        try { Console.WriteLine(string.Join(" ", listy)); }
+                        catch { Console.WriteLine("Invalid Operation!"); }
+                        break;
+                  
+                }
+            }
         }
+
+        private static ListyIterator<string> CreateListy()
+        {
+            var input = Console.ReadLine().Split();
+            if (input.Length < 2) return new ListyIterator<string>();
+            return new ListyIterator<string>(input.Skip(1));
+        }
+    
     }
 }
