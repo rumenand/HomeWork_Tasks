7f36134b346e8e532bbcf1b33ce05df71dfbf5e9
rumenand <rumen80@abv.bg>
2019-10-10 07:27:58 +0300

    Task 2 added empty

---
+++ CSharp Advanced/09.Exercises-Iterators And Comparators/Problem 1. ListyIterator/ListyIterator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem_1._ListyIterator
+{
+    class ListyIterator<T>
+    {
+        private List<T> list;
+        private int currentIndex;
+
+        public ListyIterator()
+        {
+            this.list = new List<T>();
+            this.currentIndex = 0;
+        }
+
+        public ListyIterator(IEnumerable<T> collection)
+        {
+            this.list = new List<T>(collection);
+            this.currentIndex = 0;
+        }
+
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                this.currentIndex++;
+                return true;
+            }
+            return false;
+        }
+
+        public void Print()
+        {
+            try
+            {
+                Console.WriteLine(list[currentIndex]);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+        }
+
+        public bool HasNext()
+        {
+            if (this.list.Count - 1 > currentIndex) return true;            
+            return false;
+        }
+    }
+}
--- CSharp Advanced/09.Exercises-Iterators And Comparators/Problem 1. ListyIterator/Program.cs
+++ CSharp Advanced/09.Exercises-Iterators And Comparators/Problem 1. ListyIterator/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Linq;
 
 namespace Problem_1._ListyIterator
 {
     class Program
     {
         static void Main()
-        {
-            
+        {            
+            var listy = CreateListy();
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                switch (input)
+                {                   
+                    case "Move":
+                        Console.WriteLine(listy.Move());
+                        break;
+                    case "Print":
+                        listy.Print();
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(listy.HasNext());
+                        break;
+                }
+            }
         }
+
+        private static ListyIterator<string> CreateListy()
+        {
+            var input = Console.ReadLine().Split();
+            if (input.Length <2) return new ListyIterator<string>();
+            return new ListyIterator<string>(input.Skip(1));
+        }
+        
     }
 }
