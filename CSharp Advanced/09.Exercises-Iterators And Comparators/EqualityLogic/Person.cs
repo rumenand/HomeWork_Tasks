388ba5f92cb0f3be28338f75c035abe520ecbdef
rumenand <rumen80@abv.bg>
2019-10-11 22:59:13 +0300

    Task 6 done 100%

---
+++ CSharp Advanced/09.Exercises-Iterators And Comparators/EqualityLogic/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EqualityLogic
+{
+    public class Person : IComparable<Person>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        
+        public override bool Equals(object obj)
+        {
+            Person other = obj as Person;
+            if (other == null) return false;
+            else  if (this.Name == other.Name && this.Age == other.Age) return true;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            string combined = this.Name + "|" + this.Age.ToString();
+            return combined.GetHashCode();
+        }
+
+        public int CompareTo(Person other)
+        {
+            int result = this.Name.CompareTo(other.Name);
+            if (result == 0) result = this.Age.CompareTo(other.Age);
+            return result;
+        }
+    }
+}
--- CSharp Advanced/09.Exercises-Iterators And Comparators/EqualityLogic/Program.cs
+++ CSharp Advanced/09.Exercises-Iterators And Comparators/EqualityLogic/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EqualityLogic
 {
@@ -6,7 +7,18 @@ namespace EqualityLogic
     {
         static void Main()
         {
-            
+            var hashSetPeople = new HashSet<Person>();
+            var sortedSetPeople = new SortedSet<Person>();
+            var count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                var input = Console.ReadLine().Split();
+                var newPerson = new Person(input[0], int.Parse(input[1]));
+                hashSetPeople.Add(newPerson);
+                sortedSetPeople.Add(newPerson);
+            }
+            Console.WriteLine(hashSetPeople.Count);
+            Console.WriteLine(sortedSetPeople.Count);
         }
     }
 }
