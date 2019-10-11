6f2bff465f99814241edbc911a34c3d7059f96c8
rumenand <rumen80@abv.bg>
2019-10-11 21:50:37 +0300

    Task 5 done 100%

--- CSharp Advanced/09.Exercises-Iterators And Comparators/ComparingObjects/Program.cs
+++ CSharp Advanced/09.Exercises-Iterators And Comparators/ComparingObjects/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ComparingObjects
 {
@@ -6,7 +7,21 @@ namespace ComparingObjects
     {
         static void Main()
         {
-            
+            var listPeople = new List<Person>();
+            string input;
+            while((input = Console.ReadLine()) != "END")
+            {
+                var commands = input.Split();
+                listPeople.Add(new Person(commands[0], int.Parse(commands[1]), commands[2]));
+            }
+            var comparePersonNum = int.Parse(Console.ReadLine())-1;
+            var matches = 0;
+            foreach (var person in listPeople)
+            {
+                if (listPeople[comparePersonNum].CompareTo(person) == 0) matches++;
+            }
+            if (matches < 2) Console.WriteLine("No matches");
+            else Console.WriteLine($"{matches} {listPeople.Count-matches} {listPeople.Count}");
         }
     }
 }
