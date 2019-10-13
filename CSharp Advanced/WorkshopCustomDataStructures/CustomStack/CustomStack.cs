5e0ef233ffc5d5b21bb20917afcce253779e0ebe
rumenand <rumen80@abv.bg>
2019-10-13 13:52:31 +0300

    Task 2 finished

---
+++ CSharp Advanced/WorkshopCustomDataStructures/CustomStack/CustomStack.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class CustomStack
+    {
+        private const int initialCapacity = 4;
+        private int[] items;
+        private int count;
+
+        public CustomStack()
+        {
+            this.count = 0;
+            this.items = new int[initialCapacity];
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.count;
+            }
+        }
+
+        public void Push(int element)
+        {
+            if (this.count == this.items.Length)
+            {
+                var nextItems = new int[this.count * 2];
+                for (int i = 0; i < items.Length; i++)
+                {
+                    nextItems[i] = this.items[i];
+                }
+                this.items = nextItems;
+            }
+            this.items[this.count] = element;
+            this.count++;
+        }
+
+        public int Pop()
+        {
+            if (this.items.Length == 0) throw new InvalidOperationException("CustomStack is empty");
+            this.count--;
+            return this.items[this.count];
+        }
+
+        public int Peek()
+        {
+            if (this.items.Length == 0) throw new InvalidOperationException("CustomStack is empty");
+            return this.items[count - 1];
+        }
+
+        public void ForEach(Action<object> action)
+        {
+            for (int i = 0; i < this.count; i++)
+            {
+                action(this.items[i]);
+            }
+        }
+    }
+}
--- CSharp Advanced/WorkshopCustomDataStructures/CustomStack/Program.cs
+++ CSharp Advanced/WorkshopCustomDataStructures/CustomStack/Program.cs
@@ -6,7 +6,16 @@ namespace CustomStack
     {
         static void Main()
         {
-           
+            var stack = new CustomStack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            Console.WriteLine(stack.Count);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Peek());
+            stack.ForEach(x=>Console.WriteLine(x));
         }
     }
 }
