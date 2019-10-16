13c05a9025d07bf5119e496492e952f147b55136
rumenand <rumen80@abv.bg>
2019-10-16 09:03:09 +0300

    Taks 1 updated to work with hashCodes

--- CSharp Advanced/C_sharpAdvancedRetakeExamAugust2019/SummerCocktails/Program.cs
+++ CSharp Advanced/C_sharpAdvancedRetakeExamAugust2019/SummerCocktails/Program.cs
@@ -8,15 +8,27 @@ namespace SummerCocktails
     {
         public string Name { get;}
         public int FreshnessLevel  { get;}
-        public int Count { get;set; }
         
         public Cocktail(string name, int fLevel)
         {
             this.Name = name;
-            this.FreshnessLevel = fLevel;
-            this.Count = 0;
+            this.FreshnessLevel = fLevel;         
+        }
+
+        public override bool Equals(object obj)
+        {
+            Cocktail other = obj as Cocktail;
+            if (other == null) return false;
+            else if (this.FreshnessLevel == other.FreshnessLevel) return true;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.FreshnessLevel.ToString().GetHashCode();
         }
     }
+
     class Program
     {
         static void Main()
@@ -34,20 +46,15 @@ namespace SummerCocktails
                 if (currentIngredient > 0)
                 {
                     var result = currentIngredient * freshness.Pop();
-                    bool found = false;
-                    foreach (var item in cocktailsTable)
+                    var searchCocktail = new Cocktail("", result);
+                    if (cocktailsTable.ContainsKey(searchCocktail))
                     {
-                        if (item.FreshnessLevel == result)
-                        {
-                            item.Count++;
-                            found = true;
-                            break;
-                        }
+                        cocktailsTable[searchCocktail]++;
                     }
-                    if (!found) ingredients.Enqueue(currentIngredient + 5);
+                    else ingredients.Enqueue(currentIngredient + 5);
                 }
             }
-            var sortedCocktails = cocktailsTable.OrderBy(x => x.Name).Where(x => x.Count > 0);
+            var sortedCocktails = cocktailsTable.OrderBy(x => x.Key.Name).Where(x => x.Value > 0);
             if (sortedCocktails.Count() > 3)  Console.WriteLine("It's party time! The cocktails are ready!");
             else
             {
@@ -56,18 +63,18 @@ namespace SummerCocktails
             }
             foreach(var item in sortedCocktails)
             {
-                Console.WriteLine($" # {item.Name} --> {item.Count}");
+                Console.WriteLine($" # {item.Key.Name} --> {item.Value}");
             }
 
         }
 
-        private static HashSet<Cocktail> PopulateCocktailsTable()
+        private static Dictionary<Cocktail,int> PopulateCocktailsTable()
         {
-            var set = new HashSet<Cocktail>();
-            set.Add(new Cocktail("Mimosa", 150));
-            set.Add(new Cocktail("Daiquiri", 250));
-            set.Add(new Cocktail("Sunshine", 300));
-            set.Add(new Cocktail("Mojito", 400));
+            var set = new Dictionary<Cocktail,int>();
+            set.Add(new Cocktail("Mimosa", 150),0);
+            set.Add(new Cocktail("Daiquiri", 250),0);
+            set.Add(new Cocktail("Sunshine", 300),0);
+            set.Add(new Cocktail("Mojito", 400),0);
             return set;
         }
 
