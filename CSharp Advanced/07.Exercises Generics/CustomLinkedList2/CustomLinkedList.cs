951acfb19f479633a2608c8cac776cbf6b528512
rumenand <rumen80@abv.bg>
2019-10-17 08:36:50 +0300

    Task 2 done 100%

---
+++ CSharp Advanced/07.Exercises Generics/CustomLinkedList2/CustomLinkedList.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomLinkedList2
+{
+    class CustomLinkedList<V>
+    {
+        private class ListNode<T>
+        {
+            public T Value { get; set; }
+            public ListNode<T> NextNode { get; set; }
+            public ListNode<T> PreviousNode { get; set; }
+
+            public ListNode(T value)
+            {
+                this.Value = value;
+            }
+        }
+        private ListNode<V> head;
+        private ListNode<V> tail;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(V element)
+        {
+            if (this.Count == 0)
+            {
+                this.head = this.tail = new ListNode<V>(element);
+            }
+            else
+            {
+                var newHead = new ListNode<V>(element);
+                newHead.NextNode = this.head;
+                this.head.PreviousNode = newHead;
+                this.head = newHead;
+            }
+            this.Count++;
+        }
+
+        public void AddLast(V element)
+        {
+            if (this.Count == 0)
+            {
+                this.head = this.tail = new ListNode<V>(element);
+            }
+            else
+            {
+                var newTail = new ListNode<V>(element);
+                newTail.PreviousNode = this.tail;
+                this.tail.NextNode = newTail;
+                this.tail = newTail;
+            }
+            this.Count++;
+        }
+
+        public V RemoveFirst()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            var firstElement = this.head.Value;
+            this.head = this.head.NextNode;
+            if (this.head != null)
+            {
+                this.head.PreviousNode = null;
+            }
+            else
+            {
+                this.tail = null;
+            }
+            this.Count--;
+            return firstElement;
+
+        }
+
+        public V RemoveLast()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+            var lastElement = this.tail.Value;
+            this.tail = this.tail.PreviousNode;
+            if (this.tail != null)
+            {
+                this.tail.NextNode = null;
+            }
+            else
+            {
+                this.head = null;
+            }
+            this.Count--;
+            return lastElement;
+        }
+
+        public void ForEach(Action<V> action)
+        {
+            var currentNode = this.head;
+            while (currentNode != null)
+            {
+                while (currentNode != null)
+                {
+                    action(currentNode.Value);
+                    currentNode = currentNode.NextNode;
+                }
+            }
+        }
+
+        public V[] ToArray()
+        {
+            V[] array = new V[this.Count];
+            int counter = 0;
+            var currentNode = this.head;
+            while (currentNode != null)
+            {
+                array[counter] = currentNode.Value;
+                currentNode = currentNode.NextNode;
+                counter++;
+            }
+            return array;
+        }
+    }
+}
---
+++ CSharp Advanced/C_sharpAdvancedRetakeExamAugust2019/SeashellTreasure/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SeashellTreasure
+{
+    class Program
+    {
+        static void Main()
+        {
+            const int thieftSteps = 3;
+            var collectedShells = new List<string>();
+            var stolenShells = 0;
+            int rows = int.Parse(Console.ReadLine());
+            var beachMap = new string[rows][];
+            populateMap(beachMap);
+            string input;
+            while((input = Console.ReadLine())!= "Sunset")
+            {
+                var commands = input.Split();
+                if (commands.Length > 2)
+                {
+                    int xPos = GetCoord(commands[1]);
+                    int yPos = GetCoord(commands[2]);
+                    if (commands[0] == "Collect")
+                    {
+                        if (checkInMap(beachMap, xPos, yPos) && beachMap[xPos][yPos] != "-")
+                        {
+                            collectedShells.Add(beachMap[xPos][yPos]);
+                            beachMap[xPos][yPos] = "-";
+                        }
+                    }
+                    if (commands[0] == "Steal")
+                    {
+                        for (int i = 0; i < thieftSteps + 1; i++)
+                        {
+                            if (checkInMap(beachMap, xPos, yPos))
+                            {
+                                if (beachMap[xPos][yPos] != "-")
+                                {
+                                    beachMap[xPos][yPos] = "-";
+                                    stolenShells++;
+                                }
+                                switch (commands[3])
+                                {
+                                    case "up": xPos--; break;
+                                    case "down": xPos++; break;
+                                    case "left": yPos--; break;
+                                    case "right": yPos++; break;
+                                }
+                            }
+                            else break;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < beachMap.GetLength(0); i++)
+            {
+                for (int j = 0; j < beachMap[i].GetLength(0); j++)
+                {
+                    Console.Write($"{beachMap[i][j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.Write($"Collected seashells: {collectedShells.Count} ");
+            if (collectedShells.Count > 0) Console.WriteLine($"-> {string.Join(", ",collectedShells)}");
+            else Console.WriteLine();
+            Console.WriteLine($"Stolen seashells: {stolenShells}");
+        }
+
+        private static int GetCoord(string v)
+        {
+            int number;
+            bool success = Int32.TryParse(v, out number);
+            if (success)  return number;
+            return -1;
+        }
+
+        private static bool checkInMap(string[][] beachMap, int xPos, int yPos)
+        {
+            if (xPos > -1 && xPos < beachMap.GetLength(0) && yPos > -1 && yPos < beachMap[xPos].GetLength(0)) return true;
+            else return false;
+        }
+
+        private static void populateMap(string[][] beachMap)
+        {
+            for (int i = 0; i < beachMap.GetLength(0); i++)
+            {
+                var row = Console.ReadLine().Split();
+                beachMap[i] = row;
+            }
+        }
+    }
+}
