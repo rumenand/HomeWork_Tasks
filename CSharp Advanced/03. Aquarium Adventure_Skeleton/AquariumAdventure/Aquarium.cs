6dc7cd0dcf254666fde2e7ed9358db42b1583e82
rumenand <rumen80@abv.bg>
2019-10-18 08:37:11 +0300

    Task 3 from exam -> only 77% ... need to find where is the mistake!

---
+++ CSharp Advanced/03. Aquarium Adventure_Skeleton/AquariumAdventure/Aquarium.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquariumAdventure
+{
+    public class Aquarium
+    {
+        public List<Fish> fishInPool;
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Size { get; set; }
+        private int currentFinsCount;
+
+        public Aquarium(string name, int capacity, int size)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.Size = size;
+            this.fishInPool = new List<Fish>();
+            this.currentFinsCount = 0;
+        }
+
+        public void Add(Fish fish)
+        {
+            if (GetIndexOf(fish.Name) < 0 && this.currentFinsCount<this.Size)
+            {
+                this.currentFinsCount += fish.Fins;
+                this.fishInPool.Add(fish);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            int index = GetIndexOf(name);
+            if (index > -1)
+            {
+                fishInPool.RemoveAt(index);
+                return true;
+            }
+            return false;
+        }
+
+        public Fish FindFish(string name)
+        {
+            int index = GetIndexOf(name);
+            if (index > -1)
+            {               
+                return fishInPool[index];
+            }
+            return null;
+        }
+
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            sb.Append("Aquarium Info:");
+            sb.Append(Environment.NewLine);
+            sb.Append($"Aquarium: {this.Name} ^ Size: {this.Size}");
+            sb.Append(Environment.NewLine);
+            foreach (var fish in fishInPool)
+            {
+                sb.Append(fish.ToString());
+            }
+            return sb.ToString();
+        }
+
+        private int GetIndexOf(string name)
+        {
+            return fishInPool.FindIndex(x => x.Name == name);
+        }        
+    }
+}
---
+++ CSharp Advanced/03. Aquarium Adventure_Skeleton/AquariumAdventure/Fish.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace AquariumAdventure
+{
+    public class Fish
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Fins { get; set; }
+
+        public Fish(string name, string color, int fins)
+        {
+            this.Name = name;
+            this.Color = color;
+            this.Fins = fins;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append($"Fish: {this.Name}");
+            sb.Append(Environment.NewLine);
+            sb.Append($"Color: {this.Color}");
+            sb.Append(Environment.NewLine);
+            sb.Append($"Number of fins: {this.Fins}");
+            return sb.ToString();
+        }        
+    }
+}
