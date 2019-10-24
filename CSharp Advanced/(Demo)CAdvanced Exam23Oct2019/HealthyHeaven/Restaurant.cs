43c11853ad1b6853c468fdd11d775d95ea0225cd
rumenand <rumen80@abv.bg>
2019-10-24 21:52:26 +0300

    Task 3 done 100%

---
+++ CSharp Advanced/(Demo)CAdvanced Exam23Oct2019/HealthyHeaven/Restaurant.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HealthyHeaven
+{
+    public class Restaurant
+    {
+        private List<Salad> salads;
+        public string Name { get; set; }
+
+        public Restaurant(string name)
+        {
+            this.Name = name;
+            this.salads = new List<Salad>();
+        }
+
+        public void Add(Salad salad)
+        {
+            this.salads.Add(salad);
+        }
+
+        public bool Buy(string name)
+        {
+            var index = salads.FindIndex(x => x.Name == name);
+            if (index > -1)
+            {
+                salads.RemoveAt(index);
+                return true;
+            }
+            return false;
+        }
+
+        public Salad GetHealthiestSalad()
+        {
+            return salads.OrderBy(x => x.GetTotalCalories()).FirstOrDefault();
+        }
+
+        public string GenerateMenu()
+        {
+            var sb = new StringBuilder();
+            sb.Append($"{this.Name} have {this.salads.Count} salads:");
+            sb.Append(Environment.NewLine);
+            sb.Append(string.Join(Environment.NewLine, this.salads));
+            return sb.ToString();
+        }
+    }
+}
---
+++ CSharp Advanced/(Demo)CAdvanced Exam23Oct2019/HealthyHeaven/Salad.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace HealthyHeaven
+{
+    public class Salad
+    {
+        private List<Vegetable> products;
+        public string Name { get; set; }
+
+        public Salad(string name)
+        {
+            this.Name = name;
+            this.products = new List<Vegetable>();
+        }
+
+        public int GetTotalCalories()
+        {
+            return this.products.Sum(x => x.Calories);
+        }
+
+        public int GetProductCount()
+        {
+            return this.products.Count;
+        }
+
+        public void Add(Vegetable product)
+        {
+            this.products.Add(product);
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append($"* Salad {this.Name} is {this.GetTotalCalories()} calories and have {GetProductCount()} products:");
+            sb.Append(Environment.NewLine);
+            sb.Append(string.Join(Environment.NewLine, this.products));               
+            return sb.ToString();
+        }
+    }
+}
