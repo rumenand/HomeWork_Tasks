1ca8e7161cb37633fdf69ecb08fca759e1c5d71b
rumenand <rumen80@abv.bg>
2019-10-27 18:59:19 +0200

    Task 1 done 100%

---
+++ CSharpOOP/01.Exercises Encapsulation/1ClassBoxData/Box.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _1ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+        public Box(double length, double width, double height)
+        {
+            if (length <= 0) throw new ArgumentException("Length cannot be zero or negative.");
+            if (width <= 0) throw new ArgumentException("Width cannot be zero or negative.");
+            if (height <= 0) throw new ArgumentException("Height cannot be zero or negative.");
+            this.length = length;
+            this.width = width;
+            this.height = height;
+        }        
+
+        public string SurfaceArea()
+        {
+            double result = 2 * this.length * this.width + 2 * this.length * this.height + 2 * this.width * this.height;
+            return $"Surface Area - {result:F2}";
+        }
+
+        public string LateralSurface()
+        {
+            double result = 2 * this.length * this.height + 2*this.width * this.height;
+            return $"Lateral Surface Area - {result:F2}";
+        }
+
+        public string Volume()
+        {
+            double result = this.length * this.width * this.height;
+            return $"Volume - {result:F2}";
+        }
+    }
+}
--- CSharpOOP/01.Exercises Encapsulation/1ClassBoxData/Program.cs
+++ CSharpOOP/01.Exercises Encapsulation/1ClassBoxData/Program.cs
@@ -6,7 +6,22 @@ namespace _1ClassBoxData
     {
         static void Main()
         {
-            
+            var length = double.Parse(Console.ReadLine());
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+            Box newBox = null;
+            try
+            {
+                newBox = new Box(length, width, height);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"{e.Message}");
+                return;
+            }            
+                Console.WriteLine(newBox.SurfaceArea());
+                Console.WriteLine(newBox.LateralSurface());
+                Console.WriteLine(newBox.Volume());            
         }
     }
 }
