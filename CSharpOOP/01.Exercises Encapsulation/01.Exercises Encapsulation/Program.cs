2e6d0b685ae47ce2c709010d5423841d5c5f051b
rumenand <rumen80@abv.bg>
2019-10-27 16:11:22 +0200

    Added encapsulation empty

---
+++ CSharpOOP/01.Exercises Encapsulation/01.Exercises Encapsulation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.Exercises_Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
