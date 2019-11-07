94bb227cb6184d8e42b33985cf5e255e8c54ae90
rumenand <rumen80@abv.bg>
2019-11-07 21:35:39 +0200

    Taks 6 done 100%

--- CSharpOOP/04.ExerciseInterfacesAndAbstraction/6BirthdayCelebrations/StartUp.cs
+++ CSharpOOP/04.ExerciseInterfacesAndAbstraction/6BirthdayCelebrations/StartUp.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
 namespace _6BirthdayCelebrations
 {
@@ -6,7 +9,35 @@ namespace _6BirthdayCelebrations
     {
         static void Main()
         {
-            
+            var list = new List<IBirthtable>();
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                var commands = input.Split();
+                switch (commands[0])
+                {
+                    case "Citizen":
+                        list.Add(new Citizen(commands[1], 
+                            int.Parse(commands[2]),
+                            commands[3],
+                            DateParser(commands[4])));
+                        break;
+                    case "Pet":
+                        list.Add(new Pet(commands[1], DateParser(commands[2])));
+                        break;
+                }
+            }
+            var searchYear = int.Parse(Console.ReadLine());
+            var result = list.Where(x => x.BirthDay.Year == searchYear);
+            foreach (var birthable in result)
+            {
+                Console.WriteLine(birthable.BirthDay.ToString("dd/MM/yyyy"));
+            }
+        }
+        static DateTime DateParser(string date)
+        {
+            return DateTime.ParseExact(date, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
         }
     }
 }
