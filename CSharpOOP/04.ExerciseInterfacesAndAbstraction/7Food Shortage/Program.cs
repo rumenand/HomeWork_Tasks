7ac018194b130e5c2484a06888a2d5e092114b98
rumenand <rumen80@abv.bg>
2019-11-09 22:50:29 +0200

    Task 7 added empty

---
+++ CSharpOOP/04.ExerciseInterfacesAndAbstraction/7Food Shortage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _7Food_Shortage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
