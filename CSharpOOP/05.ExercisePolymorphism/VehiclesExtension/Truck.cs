c477e59e03ad13d61936676c174eed14c71326fb
rumenand <rumen80@abv.bg>
2019-11-17 10:27:06 +0200

    Task 2 added empty

---
+++ CSharpOOP/05.ExercisePolymorphism/VehiclesExtension/Truck.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Vehicles
+{
+    public class Truck : Vehicle
+    {
+        public Truck(double fuelQuantity, double consumption) : base(fuelQuantity, consumption)
+        {
+            this.AirConditionerConsumtion = 1.6;
+            this.Name = "Truck";
+        }       
+
+        public override void RefuelTank(double refillAmount)
+        {
+            this.FuelQuantity += refillAmount * 0.95;
+        }
+    }
+}
---
+++ CSharpOOP/05.ExercisePolymorphism/VehiclesExtension/Vehicle.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Vehicles
+{
+    public abstract class Vehicle
+    {
+        public Vehicle(double fuelQuantity, double consumption)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = consumption;
+        }
+        public string Name { get; set; }
+        public double FuelQuantity { get; protected set; }
+        public double FuelConsumption { get; protected set; }
+
+        protected double AirConditionerConsumtion { get; set; }
+
+        public string Drive(double distance)
+        {
+            var fuel = this.NeededFuelForDistance(distance);
+            if (fuel > this.FuelQuantity) return $"{this.Name} needs refueling";
+            this.FuelQuantity -= fuel;
+            return $"{this.Name} travelled {distance} km";
+        }
+
+        public abstract void RefuelTank(double refillAmount);
+
+        private double NeededFuelForDistance(double distance)
+        {
+            return (this.FuelConsumption + this.AirConditionerConsumtion) * distance;
+        }
+    }
+}
