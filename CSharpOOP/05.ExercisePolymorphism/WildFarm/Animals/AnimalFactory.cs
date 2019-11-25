1874c4649b487c6999c2a325c8b343f98f805f5f
rumenand <rumen80@abv.bg>
2019-11-25 16:36:01 +0200

    Task 3 done with reflection

--- CSharpOOP/05.ExercisePolymorphism/WildFarm/Animals/AnimalFactory.cs
+++ CSharpOOP/05.ExercisePolymorphism/WildFarm/Animals/AnimalFactory.cs
@@ -3,27 +3,26 @@ using System.Collections.Generic;
 using WildFarm.Animals.Birds;
 using WildFarm.Animals.Mamals;
 using WildFarm.Animals.Mamals.Felines;
+using System.Reflection;
+using System.Linq;
 
 namespace WildFarm.Animals
 {
     public class AnimalFactory
     {
-        Dictionary<string, IAnimalCreator> createMap;
         public AnimalFactory()
         {
-            createMap = new Dictionary<string, IAnimalCreator>();
-            createMap.Add("Hen", new HenCreator());
-            createMap.Add("Owl", new OwlCreator());
-            createMap.Add("Dod", new DogCreator());
-            createMap.Add("Mouse", new MouseCreator());
-            createMap.Add("Cat", new CatCreator());
-            createMap.Add("Tiger", new TigerCreator());
+         
         }
         public Animal CreateAnimal (string [] args)
         {
-            if (createMap.ContainsKey(args[0]))
-                return createMap[args[0]].Create(args);
-            return null;
+            var currentFactory = Assembly
+             .GetExecutingAssembly()
+             .GetTypes()
+             .Where(x => x.Name == $"{args[0]}Creator")
+             .FirstOrDefault();
+            var animalCreator = (IAnimalCreator)Activator.CreateInstance(currentFactory);
+            return animalCreator.Create(args);
         }
     }
 }
