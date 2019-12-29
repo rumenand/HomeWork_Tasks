1ca2fd0d9eeb8c39e788de1b409258dad5f02ddf
rumenand <rumen80@abv.bg>
2019-12-29 19:27:49 +0200

    Added exercises JS Syntax. Task 1 done 100%

---
+++ JS Advanced/01.Exercise Syntax, Functions and Statements/1. Fruit/app.js
@@ -0,0 +1,8 @@
+
+'use strict';
+function solve(a, b, c) {
+    b = (b / 1000);
+    let money = (b * c).toFixed(2);
+    console.log(`I need \$${money} to buy ${b.toFixed(2)} kilograms ${a}.`);
+}
+solve('apple', 1563, 2.35);
