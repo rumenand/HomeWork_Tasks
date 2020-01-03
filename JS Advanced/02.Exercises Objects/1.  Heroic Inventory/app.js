3545b3a77f72651346c101d5149b496e42ac746a
rumenand <rumen80@abv.bg>
2020-01-03 21:18:46 +0200

    Added Exercise Objects

---
+++ JS Advanced/02.Exercises Objects/1.  Heroic Inventory/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function solve(a) {
+    let heroes = a.map(row =>
+        customSpliter(row, '/')).map(row => {
+            return {
+                name: row[0],
+                level: Number(row[1]),
+                items: (!row[2]) ? [] : customSpliter(row[2], ', ')
+            }
+        });
+
+    console.log(JSON.stringify(heroes));
+
+    function customSpliter(a,sp) {
+        return a.split(sp).filter((x) => x != "").map((x) => x.trim());
+    }
+}
+
+solve(['Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara']
+);
