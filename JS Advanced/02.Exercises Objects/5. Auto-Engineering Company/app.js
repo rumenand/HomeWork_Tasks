9623c8b3417241ae491fd53c0056636bb9ad44f4
rumenand <rumen80@abv.bg>
2020-01-04 20:25:50 +0200

    Added task 5

---
+++ JS Advanced/02.Exercises Objects/5. Auto-Engineering Company/app.js
@@ -0,0 +1,46 @@
+'use strict';
+function solve(a) {
+    let prod = a.map(x => x.split(" | ")).reduce((acc, cur) => {
+        let car = cur[0];
+        let model = cur[1];
+        let manif = Number(cur[2]);
+        if (car in acc) {
+            const found = acc[car].find(el => el.model == model);
+            if (found == null) acc[car].push({
+                model: model,
+                prod: manif
+            });
+            else found.prod += manif;
+        }
+        else {
+            acc[car] = [{
+                model: model,
+                prod: manif
+            }];
+        }
+        return acc;
+    }, {});   
+
+    let e = "";
+    for (const car in prod) {
+        if (e != car) {
+            e = car;
+            console.log(e);
+            for (const mod in prod[e]) {
+                console.log(`###${prod[e][mod].model} -> ${prod[e][mod].prod}`);
+            }
+        }        
+    }
+}
+
+solve(['Audi | Q7 | 1000',
+    'Audi | Q6 | 100',
+    'BMW | X5 | 1000',
+    'BMW | X6 | 100',
+    'Citroen | C4 | 123',
+    'Volga | GAZ-24 | 1000000',
+    'Lada | Niva | 1000000',
+    'Lada | Jigula | 1000000',
+    'Citroen | C4 | 22',
+    'Citroen | C5 | 10']
+);
