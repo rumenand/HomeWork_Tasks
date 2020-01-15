a709708ad595b2cde81d61e71310aa65a5ff0d35
rumenand <rumen80@abv.bg>
2020-01-15 20:41:37 +0200

    task 8 falsy upgr

--- JS Advanced/03.Exercise Arrays/8.Tic-Tac-Toe/8.Tic-Tac-Toe.js
+++ JS Advanced/03.Exercise Arrays/8.Tic-Tac-Toe/8.Tic-Tac-Toe.js
@@ -1,4 +1,12 @@
 function solve(moves){
+    const chWinner = (p,board) =>
+        (getResult(board[0],p) || getResult(board[1],p) || getResult(board[2],p)
+        || getResult(board.map(x => x[0]),p)
+        || getResult(board.map(x => x[1]),p)
+        || getResult(board.map(x => x[2]),p)
+        || getResult(board.map((x,i) => x[i]),p)
+        || getResult(board.map((x,i) => x[board.length-1-i]),p))
+        ?  true :  false;
     const initBoard = () => Array.from(Array(3), () => new Array(false,false,false));
     const hasMove = (a) => a.some(row => row.some(x=>x ===false));
     const swapPlayers = (a) => (a === 'X')?'O':'X';
@@ -9,7 +17,8 @@ function solve(moves){
     while(hasMove(board) && moves.length)
     {
         let cMove = moves.shift().split(" ");
-        if (!setOnBoard(curPl,cMove,board)){
+        if (board[cMove[0]][cMove[1]] === false) board[cMove[0]][cMove[1]] = curPl;
+        else {
             console.log("This place is already taken. Please choose another!");
             continue;
         }
@@ -19,38 +28,20 @@ function solve(moves){
         }
         curPl = swapPlayers(curPl);
     }
-    if (winner === undefined) console.log("The game ended! Nobody wins :(");
-    else console.log(`Player ${winner} wins!`);
-    board.map(x => console.log(x.join("\t")));
-    function setOnBoard(p,m,b){
-        if (b[m[0]][m[1]] === false) {
-        b[m[0]][m[1]] = p;
-        return true;
-        }
-        return false;
-    }
-    function chWinner(p,board){
-        let diag = getResult(board.map((x,i) => x[i]));
-        if(getResult(board[0],p) || getResult(board[1],p) || getResult(board[2],p)
-        || getResult(board.map(x => x[0]),p)
-        || getResult(board.map(x => x[1]),p)
-        || getResult(board.map(x => x[2]),p)
-        || getResult(board.map((x,i) => x[i]),p)
-        || getResult(board.map((x,i) => x[board.length-1-i]),p)
-        )
-        return true;
-        else false;
-    }
+    console.log((!winner) 
+    ? "The game ended! Nobody wins :("
+    : `Player ${winner} wins!`);
+    board.map(x => console.log(x.join("\t")));  
 }
 
 solve(["0 1",
 "0 0",
-"0 2",
+"0 2", 
 "2 0",
 "1 0",
-"1 2",
 "1 1",
-"2 1",
+"1 2",
 "2 2",
+"2 1",
 "0 0"]
 );
