912adf8b1c053fadf9290fb4cfe22f8deb090130
rumenand <rumen80@abv.bg>
2020-01-21 19:47:39 +0200

    Task 3 and 4

--- JS Advanced/05.Lab DOM/04. Visited Websites/solution.js
+++ JS Advanced/05.Lab DOM/04. Visited Websites/solution.js
@@ -1,4 +1,30 @@
-function solve() {
-
-  //TODO...
+function solve(e) {
+  let tg = document.getElementsByClassName('middled')[0];
+  if (!e) {
+   tg.addEventListener('click', solve);
+   var siteVists = {
+    Google:2,
+    Wikipedia:4,
+    stackoverflow:6,
+    YouTube:4,
+    SoftUni:1,
+    Gmail:7
+   };
+   tg._visits = siteVists;
+   return;
+  }
+  let siteMap = {
+    A: (x)=> x.target,
+    SPAN: (x) => x.target.parentElement
+  };
+  let visit = siteMap[e.target.nodeName];
+  if (visit !== undefined){
+    let site = visit(e).textContent.trim();
+    let visitMap = tg._visits;
+    if (visitMap[site] !== undefined){
+      visitMap[site]++;
+      let counter = visit(e).nextElementSibling;
+      counter.innerHTML = `visited ${visitMap[site]} times`; 
+    }
+  }
 }
