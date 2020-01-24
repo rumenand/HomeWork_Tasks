8f0a65cb211df84b74cc9dbc4f2ce6ee18419a64
rumenand <rumen80@abv.bg>
2020-01-24 07:38:59 +0200

    Task 6,7

--- JS Advanced/05. JS-Advanced-DOM-Exercise-Resources (1)/06. Table-Search-Engine/solution.js
+++ JS Advanced/05. JS-Advanced-DOM-Exercise-Resources (1)/06. Table-Search-Engine/solution.js
@@ -1,3 +1,20 @@
 function solve() {
-   //TODO...
+   const tBodyEl = document.querySelectorAll('tbody tr');
+   const table = [...tBodyEl]
+                .reduce((a,b,i) => {
+                a.push(b.innerText);
+                return a},[]);
+   document.getElementById('searchBtn').addEventListener('click',handler);
+   const sText = document.getElementById('searchField');     
+
+   function handler(e){    
+   [...tBodyEl].map(x=>x.classList ='');
+   [...tBodyEl].forEach(e => {
+        const text = e.textContent;
+        if (text.includes(sText.value) && sText.value !== '') {
+          e.className = 'select';
+    }
+   });
+   sText.value = '';
+   }
 }
--- JS Advanced/05. JS-Advanced-DOM-Exercise-Resources (1)/07. Shopping-Cart/solution.js
+++ JS Advanced/05. JS-Advanced-DOM-Exercise-Resources (1)/07. Shopping-Cart/solution.js
@@ -1,3 +1,23 @@
 function solve() {
-   //TODO...
+   const addBtns = [...document.querySelectorAll('.add-product')];
+   addBtns.map(x=> x.addEventListener('click',addProduct));
+   const chOutBtn = document.querySelector('.checkout');
+   chOutBtn.addEventListener('click',checkOut);
+
+   const tField = document.querySelector('textarea');
+   let totPrice = 0;
+   let products = new Set();
+
+   function addProduct(e){
+      const name = e.target.parentNode.parentNode.children[1].children[0].textContent;
+      const money = e.target.parentNode.parentNode.children[3].textContent;
+      tField.value += `Added ${name} for ${money} to the cart.\n`;
+      totPrice += Number(money);
+      products.add(name);
+   }
+   function checkOut(){
+      tField.value += `You bought ${[...products].join(", ")} for ${totPrice.toFixed(2)}.`
+      addBtns.map(x=>x.disabled = true);
+      chOutBtn.disabled = true;
+   }
 }
