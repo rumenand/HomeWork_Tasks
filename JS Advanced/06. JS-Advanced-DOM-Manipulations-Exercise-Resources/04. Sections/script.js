a2a3b2cb6526be3824dd221f094cdfcad17454a3
rumenand <rumen80@abv.bg>
2020-01-26 11:27:34 +0200

    task 4 and 5

--- JS Advanced/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/04. Sections/script.js
+++ JS Advanced/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/04. Sections/script.js
@@ -1,3 +1,12 @@
 function create(words) {
-   console.log('TODO:...');
+   let ctr = document.querySelector('#content');
+   words.map(x =>{
+      const p = document.createElement('p');
+      p.textContent = x;
+      p.style.display = 'none';
+      const a = document.createElement('div');      
+      a.appendChild(p);      
+      a.addEventListener('click', () => p.style.display = 'block');
+      ctr.appendChild(a);
+   })
 }
--- JS Advanced/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/05. Notification/scripts.js
+++ JS Advanced/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/05. Notification/scripts.js
@@ -1,3 +1,10 @@
 function notify(message) {
-    console.log('TODO:...');
+    const n = document.getElementById('notification');
+    n.innerHTML = message;
+    n.style.display = 'block';
+
+setTimeout(function(){
+    n.innerHTML = '';
+    n.style.display = 'none';
+}, 2000);
 }
