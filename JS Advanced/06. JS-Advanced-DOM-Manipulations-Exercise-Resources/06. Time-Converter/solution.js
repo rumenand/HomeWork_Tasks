d366e4d2f0124874c3032e7ded48788c981a2c42
rumenand <rumen80@abv.bg>
2020-01-27 21:36:56 +0200

    Task 6

--- JS Advanced/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/06. Time-Converter/solution.js
+++ JS Advanced/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/06. Time-Converter/solution.js
@@ -1,4 +1,24 @@
 function attachEventsListeners() {
+    const fields =  [...document.querySelectorAll('input')]
+    .filter(x=>x.type === 'text')
+    .reduce((a,b) => {
+        a.set(b.id, b);
+        return a;
+    },new Map());
+    const funcMap = {
+        daysBtn : () => Number(fields.get('days').value),
+        hoursBtn: () => Number(fields.get('hours').value)/24,
+        minutesBtn: () => Number(fields.get('minutes').value)/1440,
+        secondsBtn: () => Number(fields.get('seconds').value)/86400
+    }
+    const btn = [...document.querySelectorAll('input')]
+    .filter(x=>x.type === 'button')
+    .map(x=>x.addEventListener('click',(e) => setFields(funcMap[e.target.id]())));
 
-    console.log('TODO:...');
+    function setFields(a){
+        fields.get('days').value = a
+        fields.get('hours').value = a*24;
+        fields.get('minutes').value = a*24*60;
+        fields.get('seconds').value = a*24*60*60;
+    }
 }
