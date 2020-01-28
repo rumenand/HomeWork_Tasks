6bdac584d7b862ffac3e34076bef6641a19f5bd9
rumenand <rumen80@abv.bg>
2020-01-28 12:34:47 +0200

    Task 7

--- JS Advanced/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/07. Locked-Profile/app.js
+++ JS Advanced/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/07. Locked-Profile/app.js
@@ -1,3 +1,19 @@
 function lockedProfile() {
-    console.log('TODO...')
+    [...document.querySelectorAll('button')]
+    .map(x=>x.addEventListener('click',handler));
+    
+    function handler(e){       
+        const lock = e.target.parentNode.querySelector('input[type="radio"]:checked').value;
+        if (lock === 'unlock'){
+        const hidData =  e.target.parentNode.getElementsByTagName('div')[0];
+       if (hidData.style.display === 'block') {
+           hidData.style.display = 'none';
+           e.target.textContent = 'Show more';
+       }
+       else {
+           hidData.style.display = 'block';
+           e.target.textContent = 'Hide it';
+        }
+    }
+    }
 }
