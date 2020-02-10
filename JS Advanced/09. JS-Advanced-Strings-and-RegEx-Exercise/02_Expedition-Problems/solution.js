51bcd785709e590318cec12b7c5a5ede1f336cb8
rumenand <rumen80@abv.bg>
2020-02-10 12:43:31 +0200

    Task 2

--- JS Advanced/09. JS-Advanced-Strings-and-RegEx-Exercise/02_Expedition-Problems/solution.js
+++ JS Advanced/09. JS-Advanced-Strings-and-RegEx-Exercise/02_Expedition-Problems/solution.js
@@ -1,4 +1,27 @@
 function solve() {
-
-    //TODO...
+    const mArg = document.querySelector('#string').value;
+    const text = document.querySelector('#text').value;
+    const coordPattern  = /(east|north).*?(\d{2})[^,]*?,[^,]*?(\d{6})/gim;
+    const messagePattern = `(?<=${mArg}).+(?=${mArg})`;
+    const message = text.match(messagePattern);
+    let coords = coordPattern.exec(text);
+    let north ='',
+        east = '';
+    while(coords){        
+        const coord = coords[2]+'.'+coords[3];
+        (coords[1].toLowerCase() === 'north')
+        ? north = coord
+        : east = coord;
+        coords = coordPattern.exec(text); 
+    }
+    const result = document.querySelector('#result');
+    const msg = document.createElement('p');
+    const eCoord = document.createElement('p');
+    const nCoord = document.createElement('p');
+    msg.textContent = 'Message: '+message;
+    eCoord.textContent = east +' E';
+    nCoord.textContent = north + ' N';
+    result.appendChild(nCoord);
+    result.appendChild(eCoord);
+    result.appendChild(msg);
 }
