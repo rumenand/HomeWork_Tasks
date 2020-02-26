dd47bf4be3ab035615ce62a5863a46f043a26114
rumenand <rumen80@abv.bg>
2020-02-26 08:29:58 +0200

    Task 1-2-3

--- JS Aplicatinos/03. Exercises Prototypes and Interitance/1.Balloons.js
+++ JS Aplicatinos/03. Exercises Prototypes and Interitance/1.Balloons.js
@@ -8,7 +8,7 @@ function solve(){
     class PartyBalloon extends Balloon{
         constructor (color,gasWeight,ribbonColor,ribbonLength ){
             super(color,gasWeight);
-            this._ribbon = {ribbonColor,ribbonLength};
+            this._ribbon = {color:ribbonColor,length:ribbonLength};
     }
     get ribbon(){
         return this._ribbon;
@@ -29,9 +29,3 @@ function solve(){
         BirthdayBalloon
     }
 }
-let classes = solve();
-
-let test = new classes.Balloon("Tumno-bqlo", 20.5);
-
-
-console.log(test)
---
+++ JS Aplicatinos/03. Exercises Prototypes and Interitance/3.Posts.js
@@ -0,0 +1,67 @@
+function solve(){
+    class Post{
+        title;
+        content;
+        constructor(title, content){
+            this.title = title;
+            this.content = content;
+        }
+        toString(){
+            return `Post: ${this.title}\nContent: ${this.content}\n`
+        }
+    }
+    class SocialMediaPost extends Post{        
+        constructor(title, content,likes,dislikes){
+            super(title,content);
+            this._comments = [];
+            this._likes = likes;
+            this._dislikes = dislikes;
+        }
+        addComment(comment){
+            this._comments.push(comment);
+        }
+        toString(){ 
+            let str = super.toString();
+            str += `Rating: ${this._likes - this._dislikes}`; 
+            str += (this._comments.length>0) 
+             ?this._comments.reduce((a,b)=>{
+                a += `\n * ${b}`;
+                return a; 
+            },'\nComments:')
+            :'';
+            return str;
+            
+           
+        }
+    }
+    class BlogPost extends Post {
+        constructor(title, content,views){
+            super(title,content);
+            this._views = views;
+        }
+        view(){
+            this._views++;
+            return this;
+        }
+        toString(){
+            return super.toString()+
+            `Views: ${this._views}`
+        }
+    } 
+    return{
+        Post,SocialMediaPost,BlogPost
+    }   
+}
+let p = solve();
+let post = new p.Post("Post", "Content");
+
+console.log(post.toString());
+
+let scm = new p.SocialMediaPost("TestTitle", "TestContent", 25, 30);
+
+scm.addComment("Good post");
+scm.addComment("Very good post");
+scm.addComment("Wow!");
+
+console.log(scm.toString());
+
