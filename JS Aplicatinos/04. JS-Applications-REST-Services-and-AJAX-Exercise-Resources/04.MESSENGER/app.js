e69a029771912bcbfa3ac9bce9c0706f5790e1db
rumenand <rumen80@abv.bg>
2020-03-08 10:33:02 +0200

    task 4 upd

--- JS Aplicatinos/04. JS-Applications-REST-Services-and-AJAX-Exercise-Resources/04.MESSENGER/app.js
+++ JS Aplicatinos/04. JS-Applications-REST-Services-and-AJAX-Exercise-Resources/04.MESSENGER/app.js
@@ -1,40 +1,38 @@
 function attachEvents() {
-  document.querySelector('#refresh').addEventListener('click',refreshHandler);
-  document.querySelector('#submit').addEventListener('click',addHandler);
+  html.refrBtn().addEventListener('click',refreshHandler);
+  html.submitBtn().addEventListener('click',addHandler);
 }
-function refreshHandler(){
-    fetch(`https://rest-messanger.firebaseio.com/messanger.json`)
-    .then(res => res.json())
-    .then(printData)
-    .catch(errHandler);
+function fetchData(_,headers,callbacks){
+  fetch(getUrl(),headers)
+  .then(res=>res.json())
+  .then((data) => callbacks.forEach(x=>x(data)))
+  .catch((e) => html.msgField.value = e);
 }
+const getUrl = () => 'https://rest-messanger.firebaseio.com/messanger.json';
+const refreshHandler = () => fetchData.call(undefined,'',undefined,[printData]);
+const addHandler = () => fetchData.call(undefined,'',getAddHeader(),[refreshHandler,clearFields]);
 function printData(data){
-    const printCtn = document.querySelector('#messages');
-    printCtn.value = '';
+    html.msgField().value = '';
     const txt = (Object.keys(data)).map(x=>
          `${data[x].author}: ${data[x].content}`);
-    printCtn.value = txt.join('\n');
-    printCtn.scrollTop = printCtn.scrollHeight;
+    html.msgField().value = txt.join('\n');
+    html.msgField().scrollTop = html.msgField().scrollHeight;
 }
-function addHandler(){
-  const authorCtn = document.querySelector('#author');
-    const messageCtn = document.querySelector('#content');
-    const author = authorCtn.value;
-    const content = messageCtn.value;
-    const data = {author,content};
-    fetch(`https://rest-messanger.firebaseio.com/messanger.json`
-     ,{
-        method: 'post',
-        body: JSON.stringify(data),
-        })
-    .then(() => {
-      authorCtn.value = '';
-      messageCtn.value = '';
-    })
-    .then(refreshHandler)
+function getAddHeader(){
+    const data = {author: html.authorCtn().value,
+                  content: html.messageCtn().value};
+    return {  method: 'post',
+              body: JSON.stringify(data)};
 }
-function errHandler(err){
- document.querySelector('#messages').value = err;
+function clearFields(){
+  html.authorCtn().value = '';
+  html.messageCtn().value = '';
 }
-
+const html = {
+  refrBtn: () => document.getElementById('refresh'),
+  submitBtn: () => document.getElementById('submit'),
+  msgField: () => document.getElementById('messages'),
+  authorCtn: () => document.getElementById('author'),
+  messageCtn: () => document.getElementById('content')
+}
 attachEvents();
