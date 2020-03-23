39fb4fd2a2bbcdcfa0d0ba03bc2a32c047fecc74
rumenand <rumen80@abv.bg>
2020-03-23 21:48:45 +0200

    Task done!

--- JS Aplicatinos/07. JS-Applications-Templating-Exercise/02. HTTP Status Cats/template.js
+++ JS Aplicatinos/07. JS-Applications-Templating-Exercise/02. HTTP Status Cats/template.js
@@ -1,8 +1,30 @@
 (() => {
+    const html = {
+        list: ()=> document.getElementById('allCats'),
+        hbs:() => document.getElementById('cat-template').innerHTML,
+        btns: () => [...html.list().querySelectorAll('.showBtn')]
+    }
      renderCatTemplate();
 
      function renderCatTemplate() {
-         // TODO: Render cat template and attach events
+        const template = Handlebars.compile(html.hbs());
+        html.list().innerHTML = template({cats:window.cats});
+        attachEvents(window.cats);
      }
- 
-})
+     function attachEvents(data){
+       html.btns().forEach(btn=>{
+        btn.addEventListener('click',(e)=>{
+            const divSect = e.target.nextElementSibling;
+            if (divSect.style.display === 'none')
+            {
+                e.target.innerHTML = 'Hide status code';
+                divSect.style.display = 'block';
+            }
+            else {
+                divSect.style.display = 'none';
+                e.target.innerHTML = 'Show status code';
+            }
+        })
+       })
+     } 
+})()
--- JS Aplicatinos/07. JS-Applications-Templating-Exercise/03. Popular Monkeys/monkeysTemplate.js
+++ JS Aplicatinos/07. JS-Applications-Templating-Exercise/03. Popular Monkeys/monkeysTemplate.js
@@ -1,3 +1,30 @@
-$(() => {
-    // TODO
-})
+(() => {
+    const html = {
+        list: ()=> document.querySelector('.monkeys'),
+        hbs:() => document.getElementById('monkey-template').innerHTML,
+        btns: () => [...html.list().querySelectorAll('button')]
+    }
+     renderTemplate();
+
+     function renderTemplate() {
+        const template = Handlebars.compile(html.hbs());
+        html.list().innerHTML = template({monkeys:window.monkeys});
+       attachEvents(window.monkeys);
+     }
+     function attachEvents(data){
+       html.btns().forEach(btn=>{
+        btn.addEventListener('click',(e)=>{
+            const pSect = e.target.nextElementSibling;
+            if (pSect.style.display === 'none')
+            {
+                e.target.innerHTML = 'Hide Info';
+                pSect.style.display = 'block';
+            }
+            else {
+                pSect.style.display = 'none';
+                e.target.innerHTML = 'Info';
+            }
+        })
+       })
+     } 
+})()
